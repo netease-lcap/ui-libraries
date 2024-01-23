78dd9837054a4e6e29aa8c8e947dd64da5a2ade6
刘宇阳 <myronliu347@gmail.com>
2024-01-23 13:02:04 +0800

    chore: 创建默认stories

---
+++ libraries/pc-react-ui/src/components/Select/stories/block.stories.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Select, { SelectOption } from '../index';
+// import Select from 'antd';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+export default {
+  title: 'Example/Select/blocks',
+  component: Select,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    originDataSource: {
+      options: ['无', 'employee', 'department'],
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+    },
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const 异步函数 = {
+  render: (args) => {
+    return (
+      <Select {...args}>
+        <SelectOption key="1" label="1" value="1" />
+        <SelectOption key="2" label="2" value="2" disabled />
+      </Select>
+    );
+  },
+  args: {
+    style: { width: '256px' },
+  },
+};
