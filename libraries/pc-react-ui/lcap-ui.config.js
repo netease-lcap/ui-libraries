09ed2b3dab7c2ffb0352bc2249f916fc97d2d656
不可 <512828461@qq.com>
2024-01-31 14:46:17 +0800

    feat: add Text api.ts

--- libraries/pc-react-ui/lcap-ui.config.js
+++ libraries/pc-react-ui/lcap-ui.config.js
@@ -5,5 +5,7 @@ module.exports = {
   components: [
     { group: 'Form', name: 'Select', alias: '选择器' },
     { group: 'Table', name: 'Table', alias: '数据表格' },
+
+    { group: 'Display', name: 'Text', alias: '文本' },
   ],
 };
