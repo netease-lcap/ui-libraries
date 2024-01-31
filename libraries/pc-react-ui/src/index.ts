2f4d8869f3824f354bb540756c0d554e8d305d86
不可 <512828461@qq.com>
2024-01-31 14:55:16 +0800

    feat: add export Text

--- libraries/pc-react-ui/src/index.ts
+++ libraries/pc-react-ui/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './components/Button/index';
 export { default as Select } from './components/Select/index';
-export * from './components/Typography/index';
+export { default as Text } from './components/Text/index';
+// export * from './components/Typography/index';
