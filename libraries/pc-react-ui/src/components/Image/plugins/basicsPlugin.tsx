525dc4551456833a290e9f80bb3ff52cfa2a9863
sr2318522 <512828461@qq.com>
2024-03-05 18:08:09 +0800

    fix: img style empty

--- libraries/pc-react-ui/src/components/Image/plugins/basicsPlugin.tsx
+++ libraries/pc-react-ui/src/components/Image/plugins/basicsPlugin.tsx
@@ -8,19 +8,13 @@ export function useHandleStyle(props) {
     // const element = document.querySelector(`[data-node-id]=${nodeId}`);
     const element = document.querySelector(`[data-node-id=${nodeId}]`);
     if (!element) return;
-    // for (const key in style) {
-    //   element.style[key] = style[key];
-    // }
-    Object.entries(style).forEach(([key, value]) => {
+    _.entries(style).forEach(([key, value]) => {
       try {
         element.style[key] = style[key];
       } catch (error) {
         console.log(error);
       }
     });
-    // element?.setAttribute()
-    // element.style = style;
-    // console.log(element);
   }, [style]);
   return {
     'data-node-id': nodeId,
