4fb1ca28be2f813427494bbcf2d3812c98647a90
不可 <512828461@qq.com>
2024-03-13 14:25:01 +0800

    feat(Input): effect nodePath

--- libraries/pc-react-ui/src/components/Input/plugins/lowCode.tsx
+++ libraries/pc-react-ui/src/components/Input/plugins/lowCode.tsx
@@ -4,14 +4,16 @@ import _ from 'lodash';
 export function useHandleNodePath(props) {
   const nodeId = React.useMemo(() => _.uniqueId('input_'), []);
   const nodePath = props.get('data-nodepath');
+  const prefix = props.get('prefix');
+  const suffix = props.get('suffix');
+  const allowClear = props.get('allowClear');
   React.useEffect(() => {
     const inputElement = document.querySelector(`[data-node-id=${nodeId}]`);
     const inputParent = inputElement?.closest('.ant-input-affix-wrapper');
-    console.log(inputParent, 'input_');
     if (inputParent) {
       inputParent?.setAttribute('data-nodepath', nodePath);
     }
-  }, []);
+  }, [allowClear, suffix, prefix]);
   return {
     'data-node-id': nodeId,
   };
