da12ed0e891c75784cfc4362fc35f8c428891f78
御风 <18012261618@126.com>
2024-03-13 20:49:35 +0800

    fix: bug fix

---
+++ libraries/mobile-ui/src/fieldtextarea/demos/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <demo-section>
+    <demo-block title="基础用法">
+      <van-fieldtextarea
+        style="background-color: antiquewhite;"
+        placeholder="请输入"
+        :maxlength="500"
+        :showWordLimit="true"
+      ></van-fieldtextarea>
+    </demo-block>
+  </demo-section>
+</template>
+<script>
+export default {
+
+}
+</script>
