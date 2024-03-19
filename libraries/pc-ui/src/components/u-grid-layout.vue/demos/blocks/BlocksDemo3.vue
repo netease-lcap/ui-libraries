53522b597e616778a6e88d099363c49634838542
刘宇阳 <myronliu347@gmail.com>
2024-03-19 18:28:58 +0800

    fix(GridLayout): 增加换行默认参数

--- libraries/pc-ui/src/components/u-grid-layout.vue/demos/blocks/BlocksDemo3.vue
+++ libraries/pc-ui/src/components/u-grid-layout.vue/demos/blocks/BlocksDemo3.vue
@@ -3,8 +3,8 @@
 <template>
   <u-grid-layout>
     <u-grid-layout-row :repeat="24">
-        <u-grid-layout-column :span="8"></u-grid-layout-column>
-        <u-grid-layout-column :span="16"></u-grid-layout-column>
+        <u-grid-layout-column :span="8" :wrap="true"></u-grid-layout-column>
+        <u-grid-layout-column :span="16" :wrap="true"></u-grid-layout-column>
     </u-grid-layout-row>
 </u-grid-layout>
 </template>
