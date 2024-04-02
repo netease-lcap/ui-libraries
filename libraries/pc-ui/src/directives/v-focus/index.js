4078587429b5085422b0690e848296c2a5c758c4
changpei <changpei01@corp.netease.com>
2024-04-02 16:42:36 +0800

    fix(input): 解决drawer嵌套autofocus-input动画抖动问题

--- libraries/pc-ui/src/directives/v-focus/index.js
+++ libraries/pc-ui/src/directives/v-focus/index.js
@@ -2,9 +2,13 @@ import { select } from '../../utils/edit';
 
 export const focus = {
     inserted(el, binding) {
-        if (!!binding.value || binding.value === undefined)
-            el.focus();
-        if (binding.value === 'select')
+        if (!!binding.value || binding.value === undefined){
+            setTimeout(() => {
+                el.focus();
+            }, 300)
+        }
+        if (binding.value === 'select'){
             select(el);
+        }
     },
 };
