1a2f9c70d020f7478827c8d37b8cb4e1c3d9e300
刘宇阳 <myronliu347@gmail.com>
2024-04-11 11:07:47 +0800

    chore(ci): 不需要生成 api.ts

--- packages/builder/src/build/gen-nasl-logics-config.ts
+++ packages/builder/src/build/gen-nasl-logics-config.ts
@@ -3,16 +3,14 @@ import traverse from '@babel/traverse';
 import glob from 'fast-glob';
 import path from 'path';
 import fs from 'fs-extra';
-import { snakeCase } from 'lodash';
 import transformFunc2NaslLogic from '../nasl/transform-func2nasl-logic';
-import { getNodeCode } from '../utils/babel-utils';
 
 export default async function getNaslExtensionConfig(rootPath) {
   const logicDir = path.join(rootPath, 'src/logics');
-  const pkgInfo = fs.readJSONSync(`${rootPath}/package.json`);
+  // const pkgInfo = fs.readJSONSync(`${rootPath}/package.json`);
   const tsFiles = await glob(`${logicDir}/**/*.ts`);
   const logics: any[] = [];
-  const apiFuncCodes: string[] = [];
+  // const apiFuncCodes: string[] = [];
 
   tsFiles.forEach((tsPath) => {
     if (tsPath.endsWith('api.ts')) {
@@ -29,39 +27,39 @@ export default async function getNaslExtensionConfig(rootPath) {
         const logic = transformFunc2NaslLogic(p.node);
         if (logic) {
           logics.push(logic);
-          p.traverse({
-            FunctionDeclaration(np) {
-              np.node.generator = false;
-              np.node.async = false;
-              np.node.body.body = [];
-              np.node.params = np.node.params.map((param) => {
-                if (param.type === 'AssignmentPattern') {
-                  return param.left;
-                }
+          // p.traverse({
+          //   FunctionDeclaration(np) {
+          //     np.node.generator = false;
+          //     np.node.async = false;
+          //     np.node.body.body = [];
+          //     np.node.params = np.node.params.map((param) => {
+          //       if (param.type === 'AssignmentPattern') {
+          //         return param.left;
+          //       }
 
-                return param;
-              }) as any[];
+          //       return param;
+          //     }) as any[];
 
-              let functionCode = getNodeCode(np.node);
-              const endIndex = functionCode.lastIndexOf(' {}');
+          //     let functionCode = getNodeCode(np.node);
+          //     const endIndex = functionCode.lastIndexOf(' {}');
 
-              if (endIndex !== -1) {
-                functionCode = functionCode.substring(0, endIndex);
-              }
+          //     if (endIndex !== -1) {
+          //       functionCode = functionCode.substring(0, endIndex);
+          //     }
 
-              apiFuncCodes.push(`  export ${functionCode};`);
-            },
-          });
+          //     apiFuncCodes.push(`  export ${functionCode};`);
+          //   },
+          // });
         }
       },
     });
   });
 
-  fs.writeFileSync(`${logicDir}/api.ts`, [
-    '/// <reference types="@nasl/types" />',
-    `declare namespace extensions.${snakeCase(pkgInfo.name)}.logics {`,
-    apiFuncCodes,
-    '}',
-  ].flat().join('\n\n'));
+  // fs.writeFileSync(`${logicDir}/api.ts`, [
+  //   '/// <reference types="@nasl/types" />',
+  //   `declare namespace extensions.${snakeCase(pkgInfo.name)}.logics {`,
+  //   apiFuncCodes,
+  //   '}',
+  // ].flat().join('\n\n'));
   return logics;
 }
