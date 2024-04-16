f2062f42ac25890a0d176d0c054ba2d1489cfa37
御风 <18012261618@126.com>
2024-04-16 14:35:33 +0800

    fix(mobile-ui): 调整datetime值类型

--- libraries/mobile-ui/src/datetime-picker/api.ts
+++ libraries/mobile-ui/src/datetime-picker/api.ts
@@ -191,13 +191,13 @@ namespace nasl.ui {
       sync: true,
       if: _ => _.range === true
     })
-    startValue: nasl.core.String;
+    startValue: nasl.core.String | nasl.core.DateTime | nasl.core.Date;
     @Prop<VanDatetimePickerOptions, 'endValue'>({
       title: '结束值',
       sync: true,
       if: _ => _.range === true
     })
-    endValue: nasl.core.String;
+    endValue: nasl.core.String | nasl.core.DateTime | nasl.core.Date;
     @Prop({
       title: '是否使用新版外观',
       description: '是否使用新版外观',
@@ -213,19 +213,19 @@ namespace nasl.ui {
       sync: true,
       if: _ => _.range !== true
     })
-    value: nasl.core.String;
+    value: nasl.core.String | nasl.core.DateTime | nasl.core.Date;
     @Prop({
       group: '数据属性',
       title: '最小日期',
       description: '当时间选择类型为datetime时可选的最小时间，精确到分钟, 默认为十年前'
     })
-    minDate: nasl.core.String;
+    minDate: nasl.core.String | nasl.core.DateTime | nasl.core.Date;
     @Prop({
       group: '数据属性',
       title: '最大日期',
       description: '当时间选择类型为datetime时可选的最大时间，精确到分钟, 默认为十年后'
     })
-    maxDate: nasl.core.String;
+    maxDate: nasl.core.String | nasl.core.DateTime | nasl.core.Date;
     @Prop<VanDatetimePickerOptions, 'maxHour'>({
       group: '数据属性',
       title: '最大小时',
