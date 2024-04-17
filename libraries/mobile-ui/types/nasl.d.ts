fa30fda349e28e1236e6e9c46d6a4074e0ee6996
御风 <18012261618@126.com>
2024-04-17 10:51:27 +0800

    fix(mobile-ui): 修复缺失类型

--- libraries/mobile-ui/types/nasl.d.ts
+++ libraries/mobile-ui/types/nasl.d.ts
@@ -2,4 +2,14 @@
 
 namespace nasl.ui {
   export class BaseEvent {}
+  export class ValidateResult {
+    rawValue: nasl.core.String;
+    value: nasl.core.String;
+    trigger: nasl.core.String;
+    muted: nasl.core.String;
+    valid: nasl.core.Boolean;
+    touched: nasl.core.Boolean;
+    dirty: nasl.core.Boolean;
+    firstError: nasl.core.String;
+  }
 }
