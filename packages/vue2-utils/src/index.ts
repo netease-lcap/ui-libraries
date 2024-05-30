4fa120ab67c25955b753ab30cd5995ded897cc0a
刘宇阳 <myronliu347@gmail.com>
2024-05-30 18:08:17 +0800

    Merge branch 'test-v3.9.x' into feature/accessibility

--- packages/vue2-utils/src/index.ts
+++ packages/vue2-utils/src/index.ts
@@ -1,4 +1,12 @@
-export { install } from './install';
+export {
+  install,
+  installOptions,
+  installValidators,
+  installComponents,
+  installDirectives,
+  installFilters,
+  installRules,
+} from './install';
 export {
   sync,
 } from './mixins';
