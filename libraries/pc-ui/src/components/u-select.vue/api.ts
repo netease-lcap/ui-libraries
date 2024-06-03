cbe746a0969a3a99e63e3d658f311d95e0d857be
sr2318522 <512828461@qq.com>
2024-06-03 17:33:05 +0800

    feat(Select): empty value

--- libraries/pc-ui/src/components/u-select.vue/api.ts
+++ libraries/pc-ui/src/components/u-select.vue/api.ts
@@ -142,6 +142,17 @@ namespace nasl.ui {
 
         @Prop({
             group: '数据属性',
+            title: '空值为null',
+            description: '清空值时是否设置为null',
+            designerValue: true,
+            setter: {
+                concept: 'SwitchSetter',
+            },
+        })
+        emptyValueIsNull: nasl.core.Boolean = true;
+
+        @Prop({
+            group: '数据属性',
             title: '分页加载更多',
             description: '设置是否分页加载更多',
             docDescription: '是否使用分页功能加载更多',
