1950f79e8e7f378719aed8a5389e9dc1f576cc09
sr2318522 <512828461@qq.com>
2024-06-13 09:29:06 +0800

    feat(AbsoluteLayout): add component

--- libraries/pc-react-ui/src/components/AbsoluteLayout/stories/block.stories.jsx
+++ libraries/pc-react-ui/src/components/AbsoluteLayout/stories/block.stories.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Layout } from '../index';
+import { AbsoluteLayout } from '../index';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
   title: 'Example/Layout1/blocks',
-  component: Layout,
+  component: AbsoluteLayout,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -24,6 +24,6 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const 默认 = {
   render: () => {
-    return <Layout children="标签" />;
+    return <AbsoluteLayout />;
   },
 };
