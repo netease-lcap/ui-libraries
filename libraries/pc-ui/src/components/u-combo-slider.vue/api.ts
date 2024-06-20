a2edb09f4d7b175d284ae53e39ec0f6933ef3842
刘宇阳 <myronliu347@gmail.com>
2024-06-20 11:55:51 +0800

    fix(api): 修正list api 配置

--- libraries/pc-ui/src/components/u-combo-slider.vue/api.ts
+++ libraries/pc-ui/src/components/u-combo-slider.vue/api.ts
@@ -43,7 +43,7 @@ namespace nasl.ui {
             sync: true,
             docDescription: '滑块的值',
         })
-        value: nasl.core.Decimal | nasl.core.Integer | nasl.collection.List<nasl.core.Integer | nasl.core.Decimal> = 0;
+        value: nasl.core.Decimal | nasl.core.Integer | nasl.collection.List<nasl.core.Integer> | nasl.collection.List<nasl.core.Decimal> = 0;
 
         @Prop({
             group: '数据属性',
@@ -93,7 +93,7 @@ namespace nasl.ui {
             description: '进一步对`value`限制，通常传入一个数组，第一个值表示范围开始值，第二个值表示范围的结束值',
             docDescription: '进一步对`值`限制，通常传入一个数组，第一个值表示范围开始值，第二个值表示范围的结束值',
         })
-        range: nasl.collection.List<nasl.core.Integer | nasl.core.Decimal> = [];
+        range: nasl.collection.List<nasl.core.Integer> | nasl.collection.List<nasl.core.Decimal> = [];
 
         @Prop({
             group: '数据属性',
