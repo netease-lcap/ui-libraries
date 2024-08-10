5451036fbce17c0dac1ab9aa73d52a13a9400be4
刘宇阳 <myronliu347@gmail.com>
2024-08-10 20:55:07 +0800

    style(Element):  eslint fix

--- libraries/element-ui/src/components/el-dialog/plugins/basic-plugins.ts
+++ libraries/element-ui/src/components/el-dialog/plugins/basic-plugins.ts
@@ -1,44 +1,45 @@
 /* 组件功能扩展插件 */
-import type { NaslComponentPluginOptions, Slot } from '@lcap/nasl-hoc-vue/plugin';
-import { isNullOrUndefined } from '../../../plugins/utils';
+import type { NaslComponentPluginOptions } from '@lcap/nasl-hoc-vue/plugin';
 import { $ref } from '@lcap/nasl-hoc-vue/index';
-export const useDialog: NaslComponentPluginOptions = {
-    setup: (props) => {
-        // 同时响应外部  visible属性设置的变化
-        const opened = props.useRef('visible', (v) => (isNullOrUndefined(v) ? false : true));
+import { isNullOrUndefined } from '../../../plugins/utils';
 
-        return {
-        'visible': opened,
-        [$ref]: {
-            // 外部可以读取 visible
-            props: ['visible'],
-            // ide 内使用
-            designerControl() {
-            opened.value = !opened;
-            },
-            open: () => {
-            opened.value = true;
-            },
-            close: () => {
-            opened.value = false;
-            }
-        }
-        };
-    },
-    order: 1
+export const useDialog: NaslComponentPluginOptions = {
+  setup: (props) => {
+    // 同时响应外部  visible属性设置的变化
+    const opened = props.useRef('visible', (v) => (!isNullOrUndefined(v)));
+
+    return {
+      visible: opened,
+      [$ref]: {
+        // 外部可以读取 visible
+        props: ['visible'],
+        // ide 内使用
+        designerControl() {
+          opened.value = !opened;
+        },
+        open: () => {
+          opened.value = true;
+        },
+        close: () => {
+          opened.value = false;
+        },
+      },
+    };
+  },
+  order: 1,
 };
 
 export const useEvents = {
-    setup: (props) => {
-      return {
-        beforeClose: (done) => {
-          const onBeforeClose = props.get('onBeforeClose');
-          if (typeof onBeforeClose === 'function') {
-            onBeforeClose(done);
-          }
-        },
+  setup: (props) => {
+    return {
+      beforeClose: (done) => {
+        const onBeforeClose = props.get('onBeforeClose');
+        if (typeof onBeforeClose === 'function') {
+          onBeforeClose(done);
+        }
+      },
 
-      };
-    },
-    order: 2,
-  }
+    };
+  },
+  order: 2,
+};
