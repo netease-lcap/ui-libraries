d132967e6d0a6ef925c88b5547c4a50082b98d50
刘宇阳 <myronliu347@gmail.com>
2024-08-19 19:06:41 +0800

    feat(ElDropdown): 下拉菜单

--- libraries/element-ui/src/components/el-dropdown/index.ts
+++ libraries/element-ui/src/components/el-dropdown/index.ts
@@ -2,6 +2,7 @@ import Dropdown from 'element-ui/lib/dropdown';
 import DropdownMenu from 'element-ui/lib/dropdown-menu';
 import DropdownItem from 'element-ui/lib/dropdown-item';
 import { registerComponent } from '@lcap/nasl-hoc-vue/index';
+import * as itemPlugins from './plugins/item-plugins';
 import * as plugins from './plugins';
 
 export const ElDropdown = registerComponent(Dropdown, plugins, {
@@ -10,6 +11,10 @@ export const ElDropdown = registerComponent(Dropdown, plugins, {
   methodNames: [],
 });
 export const ElDropdownMenu = DropdownMenu;
-export const ElDropdownItem = DropdownItem;
+export const ElDropdownItem = registerComponent(DropdownItem, itemPlugins, {
+  nativeEvents: [],
+  slotNames: ['default'],
+  methodNames: [],
+});
 
 export default ElDropdown;
--- libraries/element-ui/src/components/el-dropdown/plugins/basic-plugins.ts
+++ libraries/element-ui/src/components/el-dropdown/plugins/basic-plugins.ts
@@ -1,2 +1,22 @@
 /* 组件功能扩展插件 */
-export {};
+import DropdownMenu from 'element-ui/lib/dropdown-menu';
+import { NaslComponentPluginOptions } from '@lcap/nasl-hoc-vue/index';
+
+export const useExtendsPlugin: NaslComponentPluginOptions = {
+  setup: (props, { h }) => {
+    return {
+      slotDropdown: () => {
+        const [slotDropdown, slotItems] = props.get(['slotDropdown', 'slotItems']);
+
+        const itemVNodes = typeof slotItems === 'function' ? slotItems() : [];
+        if (!slotItems || !Array.isArray(itemVNodes) || itemVNodes.length === 0) {
+          return typeof slotDropdown === 'function' ? slotDropdown() : [];
+        }
+
+        return [
+          h(DropdownMenu, {}, itemVNodes),
+        ];
+      },
+    };
+  },
+};
