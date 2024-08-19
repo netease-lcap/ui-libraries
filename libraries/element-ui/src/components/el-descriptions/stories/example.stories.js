151283cf5ae9954dab3cb31852d578652780517e
御风 <18012261618@126.com>
2024-08-19 18:21:47 +0800

    feat: 描述列表组件

--- libraries/element-ui/src/components/el-descriptions/stories/example.stories.js
+++ libraries/element-ui/src/components/el-descriptions/stories/example.stories.js
@@ -19,13 +19,26 @@ export const Example1 = {
   name: '基础用法',
   render: () => ({
     template: `<div><el-descriptions title="用户信息">
-    <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-    <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-    <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-    <el-descriptions-item label="备注">
+    <el-descriptions-item>
+      <template #label>用户名</template>
+      kooriookami
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>手机号</template>
+      18100000000
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>居住地</template>
+      苏州市
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>备注</template>
       <el-tag size="small">学校</el-tag>
     </el-descriptions-item>
-    <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>联系地址</template>
+      江苏省苏州市吴中区吴中大道 1188 号
+    </el-descriptions-item>
 </el-descriptions></div>`,
   }),
 };
@@ -45,25 +58,27 @@ export const Example2 = {
 export const Example3 = {
   name: '垂直列表',
   render: () => ({
-    template: `<div><el-descriptions title="垂直带边框列表" direction="vertical" :column="4" border>
-  <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-  <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-  <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
-  <el-descriptions-item label="备注">
-    <el-tag size="small">学校</el-tag>
-  </el-descriptions-item>
-  <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
-</el-descriptions>
+    template: `<div>
+    <el-descriptions title="垂直带边框列表" direction="vertical" :column="4" border>
+      <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+      <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
+      <el-descriptions-item label="备注">
+        <el-tag size="small">学校</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+    </el-descriptions>
 
-<el-descriptions class="margin-top" title="垂直无边框列表" :column="4" direction="vertical">
-  <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-  <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-  <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
-  <el-descriptions-item label="备注">
-    <el-tag size="small">学校</el-tag>
-  </el-descriptions-item>
-  <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
-</el-descriptions></div>`,
+    <el-descriptions class="margin-top" title="垂直无边框列表" :column="4" direction="vertical">
+      <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
+      <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+      <el-descriptions-item label="居住地" :span="2">苏州市</el-descriptions-item>
+      <el-descriptions-item label="备注">
+        <el-tag size="small">学校</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+    </el-descriptions>
+</div>`,
   }),
 };
 
