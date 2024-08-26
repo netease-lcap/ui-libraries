045d6b5fdabbca282bbbe624abf4fd3be20729d0
sr2318522 <512828461@qq.com>
2024-08-26 10:47:22 +0800

    feat: link to

--- libraries/element-pro/src/components/el-table-pro/stories/example.stories.js
+++ libraries/element-pro/src/components/el-table-pro/stories/example.stories.js
@@ -9,42 +9,50 @@ export default {
     layout: 'padded',
   },
 };
-const initialData = [];
-const total = 5;
-for (let i = 0; i < total; i++) {
-  initialData.push({
-    index: i,
-    applicant: ['贾明', '张三', '王芳'][i % 3],
-    status: i % 3,
-    channel: ['电子签署', '纸质签署', '纸质签署'][i % 3],
-    detail: {
-      email: ['w.cezkdudy@lhll.au', 'r.nmgw@peurezgn.sl', 'p.cumx@rampblpa.ru'][
-        i % 3
-      ],
-    },
-    matters: [
-      '宣传物料制作费用',
-      'algolia 服务报销',
-      '相关周边制作费',
-      '激励奖品快递费',
-    ][i % 4],
-    time: [2, 10, 1][i % 3],
-    createTime: [
-      '2022-01-01',
-      '2022-02-01',
-      '2022-03-01',
-      '2022-04-01',
-      '2022-05-01',
-    ][i % 4],
-  });
-}
 
 export const Default = {
   name: '基础示例',
   render: () => ({
     data() {
       return {
-        data: initialData,
+        data: async (params) => {
+          console.log(params, 'params');
+          const initialData = [];
+          const total = 50;
+          for (let i = 0; i < total; i++) {
+            initialData.push({
+              index: i,
+              applicant: ['贾明', '张三', '王芳'][i % 3],
+              status: i % 3,
+              channel: ['电子签署', '纸质签署', '纸质签署'][i % 3],
+              detail: {
+                email: [
+                  'w.cezkdudy@lhll.au',
+                  'r.nmgw@peurezgn.sl',
+                  'p.cumx@rampblpa.ru',
+                ][i % 3],
+              },
+              matters: [
+                '宣传物料制作费用',
+                'algolia 服务报销',
+                '相关周边制作费',
+                '激励奖品快递费',
+              ][i % 4],
+              time: [2, 10, 1][i % 3],
+              createTime: [
+                '2022-01-01',
+                '2022-02-01',
+                '2022-03-01',
+                '2022-04-01',
+                '2022-05-01',
+              ][i % 4],
+            });
+          }
+          return {
+            list: initialData,
+            total,
+          };
+        },
         columns: [
           { colKey: 'applicant', title: '申请人', width: 100 },
           { colKey: 'status', title: '状态', width: 100 },
@@ -56,14 +64,21 @@ export const Default = {
         ],
       };
     },
+    methods: {
+      log(value) {
+        console.log(value);
+      },
+    },
     template: `<el-table-pro
     row-key="index"
-    :data="data" 
+    :dataSource="data" 
     >
       <el-table-column-pro title="申请人" >
-      <template #cell="cell">
-        <div>{{ cell.row.applicant }}</div>
+        <template #cell="cell">
+          <div>{{ cell.row.applicant }}</div>
         </template>
+      </el-table-column-pro>
+      <el-table-column-pro title="渠道" colKey="channel" >
       </el-table-column-pro>
     </el-table-pro>`,
   }),
