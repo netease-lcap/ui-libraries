fcce886ea83451434bf8be51736d6bfa336a19cb
刘宇阳 <myronliu347@gmail.com>
2024-09-09 13:31:33 +0800

    feat(ElementPro): add theme vars files

---
+++ libraries/element-pro/src/theme/components/el-select-pro/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <demo-preview></demo-preview>
+</template>
+<script>
+import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
+import * as stories from '../../../components/el-select-pro/stories/block.stories';
+
+const DemoPreview = createStoriesPreview(stories);
+
+export default {
+  components: {
+    DemoPreview,
+  },
+};
+
+</script>
