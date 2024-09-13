6b0e5d05fd1efe34c449e841796bc52de31e6ce9
lidan <lidan23@corp.netease.com>
2024-09-13 13:41:35 +0800

    feat(Rate&Textarea): 主题样式 preview

--- libraries/element-pro/src/theme/components/el-rate-pro/index.vue
+++ libraries/element-pro/src/theme/components/el-rate-pro/index.vue
@@ -1,16 +1,4 @@
 <template>
-  <demo-preview></demo-preview>
+  <el-rate-pro showText></el-rate-pro>
 </template>
 <script>
-import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
-import * as stories from '../../../components/el-rate-pro/stories/block.stories';
-
-const DemoPreview = createStoriesPreview(stories);
-
-export default {
-  components: {
-    DemoPreview,
-  },
-};
-
-</script>
