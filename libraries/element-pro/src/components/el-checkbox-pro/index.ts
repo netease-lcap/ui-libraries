5a4c83dabdd8381d2091a339417d1d073dc8add0
violarong <hzzhxr@corp.netease.com>
2024-09-13 16:51:47 +0800

    fix(ElCheckboxPro|ElRadioPro): 字体颜色

--- libraries/element-pro/src/components/el-checkbox-pro/index.ts
+++ libraries/element-pro/src/components/el-checkbox-pro/index.ts
@@ -1,6 +1,7 @@
 import { Checkbox, CheckboxGroup } from '@element-pro';
 import { registerComponent } from '@lcap/vue2-utils';
 import * as plugins from './plugins';
+import './index.less';
 
 export const ElCheckboxGroupPro = registerComponent(CheckboxGroup, plugins, {
   nativeEvents: [],
