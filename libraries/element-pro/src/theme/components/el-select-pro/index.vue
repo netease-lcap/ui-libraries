402059e1682719f1af52c662286a58d7f27c584d
lidan <lidan23@corp.netease.com>
2024-09-14 18:51:57 +0800

    feat: 时间选择/日期选择/级联/选择器/穿梭框 主题样式 preview

--- libraries/element-pro/src/theme/components/el-select-pro/index.vue
+++ libraries/element-pro/src/theme/components/el-select-pro/index.vue
@@ -1,16 +1,77 @@
 <template>
-  <demo-preview></demo-preview>
+  <div>
+    <el-radio-group-pro v-model="size">
+      <el-radio-pro value="small">小尺寸</el-radio-pro>
+      <el-radio-pro value="medium">中尺寸</el-radio-pro>
+      <el-radio-pro value="large">大尺寸</el-radio-pro>
+    </el-radio-group-pro>
+    <div class="actions-warp">
+      <el-checkbox-pro v-model="disabled">是否禁用</el-checkbox-pro>
+      <el-checkbox-pro v-model="multiple">是否多选</el-checkbox-pro>
+      <el-checkbox-pro v-model="borderless">是否无边框</el-checkbox-pro>
+      <el-checkbox-pro v-model="emptyData">是否置空数据</el-checkbox-pro>
+    </div>
+
+    <el-select-pro clearable filterable :size="size" :disabled="disabled" :multiple="multiple" :borderless="borderless"
+      :dataSource="emptyData ? [] : options2" />
+
+    <el-select-pro :size="size" :disabled="disabled" :multiple="multiple" :borderless="borderless"
+      :dataSource="emptyData ? [] : options" placeholder="分组选择器" filterable clearable style="margin-left: 10px;" />
+  </div>
 </template>
 <script>
-import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
-import * as stories from '../../../components/el-select-pro/stories/block.stories';
-
-const DemoPreview = createStoriesPreview(stories);
 
 export default {
-  components: {
-    DemoPreview,
+  data() {
+    return {
+      size: 'medium',
+      disabled: false,
+      multiple: false,
+      emptyData: false,
+      borderless: false,
+      value1: '',
+      options: [
+        {
+          group: '分组一',
+          children: [
+            { label: '选项一', value: 1 },
+            { label: '选项二', value: 2 },
+          ],
+        },
+        {
+          group: '分组二',
+          children: [
+            { label: '选项三', value: 4 },
+            { label: '选项四', value: 5 },
+            { label: '选项五', value: 6 },
+          ],
+        },
+        {
+          group: '分组三',
+          divider: true,
+          children: [
+            { label: '选项六', value: 7 },
+            { label: '选项七', value: 8 },
+            { label: '选项八', value: 9 },
+          ],
+        },
+      ],
+      options2: [
+        { label: 'Apple', value: 'Apple' },
+        { label: 'Orange', value: 'Orange' },
+        { label: 'Banana', value: 'Banana', disabled: true },
+      ]
+    }
   },
-};
-
+}
 </script>
+
+<style scoped>
+.actions-warp {
+  font-size: 12px;
+  margin: 10px 0 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+</style>
--- libraries/element-pro/src/theme/components/el-time-picker-pro/index.vue
+++ libraries/element-pro/src/theme/components/el-time-picker-pro/index.vue
@@ -1,16 +1,29 @@
 <template>
-  <demo-preview></demo-preview>
+  <div>
+    <el-checkbox-pro v-model="disabled">是否禁用</el-checkbox-pro>
+
+    <div class="picker-warp">
+      <el-time-picker-pro allowInput :disabled="disabled" />
+      <el-time-range-picker-pro clearable v-model="time" format="HH:mm:ss" allowInput :disabled="disabled" />
+    </div>
+  </div>
 </template>
 <script>
-import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
-import * as stories from '../../../components/el-time-picker-pro/stories/block.stories';
-
-const DemoPreview = createStoriesPreview(stories);
 
 export default {
-  components: {
-    DemoPreview,
+  data() {
+    return {
+      disabled: false,
+      time: ['00:00:00', '23:59:59'],
+    }
   },
-};
-
+}
 </script>
+
+<style scoped>
+.picker-warp {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
