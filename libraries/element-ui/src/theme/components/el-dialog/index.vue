e2cffa3aceb4d1055f4175a2ee6f9fe029170990
lidan <lidan23@corp.netease.com>
2024-09-18 15:57:55 +0800

    fix: 修改主题样式问题

--- libraries/element-ui/src/theme/components/el-dialog/index.vue
+++ libraries/element-ui/src/theme/components/el-dialog/index.vue
@@ -1,12 +1,19 @@
 <template>
-  <demo-preview></demo-preview>
+  <el-dialog title="提示" :visible="true" width="30%" :modal="false" :show-close="false"
+    :appendToBody="false" :closeOnPressEscape="false" :closeOnClickModal="false" class="dialog-preview-wrapper">
+    <span>这是一段信息</span>
+    <span slot="footer" class="dialog-footer">
+      <el-button>取 消</el-button>
+      <el-button type="primary">确 定</el-button>
+    </span>
+  </el-dialog>
 </template>
-<script>
-import DemoPreview from '../../../components/el-dialog/demos/example-demo1.vue';
-export default {
-  components: {
-    DemoPreview,
-  },
-};
+<style>
+.dialog-preview-wrapper {
+  position: unset !important;
+}
 
-</script>
+.dialog-preview-wrapper .el-dialog {
+  margin: 10px !important;
+}
+</style>
