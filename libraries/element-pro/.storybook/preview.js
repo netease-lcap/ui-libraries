fb3719f60011dd9c8e7b55f623554baf2c74b9c8
刘宇阳 <myronliu347@gmail.com>
2024-09-18 16:16:12 +0800

    chore: add preivew css

--- libraries/element-pro/.storybook/preview.js
+++ libraries/element-pro/.storybook/preview.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import * as ElementUI from '@/main';
 import { Button } from '@element-pro';
-import './reset.css';
+import '../src/styles/preview.css';
 
 
 Vue.use(ElementUI);
@@ -16,13 +16,30 @@ Vue.component('el-text', {
   },
 });
 
+Vue.component('el-icon', {
+  name: 'ElIcon',
+  props: {
+    name: String,
+  },
+  render(h) {
+    return h('i', {
+      class: `el-icon-${this.name} el-p-icon`,
+      on: this.$listeners,
+    });
+  }
+});
+
 Vue.component('el-button', {
   props: {
     text: String,
+    icon: String,
   },
   render(h) {
     return h(Button, {
       attrs: this.$attrs,
+      props: {
+        icon: this.icon ? h('el-icon', { attrs: { name: this.icon } }) : null,
+      },
       on: this.$listeners,
     }, this.text || this.$slots.default);
   }
