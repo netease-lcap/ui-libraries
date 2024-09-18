b48f15072c00f4fe3c44be3d59a71dc47763ea0f
lidan <lidan23@corp.netease.com>
2024-09-18 17:37:42 +0800

    feat: 新增单选框&多选框主题样式 preview

--- libraries/element-pro/src/theme/components/el-checkbox-group-pro/index.vue
+++ libraries/element-pro/src/theme/components/el-checkbox-group-pro/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <demo-preview></demo-preview>
+  <div>
+    <el-checkbox-group-pro :checkAll="true">
+      <el-checkbox-pro key="1" value="1">选项一</el-checkbox-pro>
+      <el-checkbox-pro key="2" value="2">选项二</el-checkbox-pro>
+      <el-checkbox-pro key="3" value="3">选项三</el-checkbox-pro>
+    </el-checkbox-group-pro>
+    <div style="display: flex; gap: 15px;">
+      <el-checkbox-pro key="4" value="4" disabled>未选中-禁用</el-checkbox-pro>
+      <el-checkbox-pro key="5" value="5" checked disabled>选中-禁用</el-checkbox-pro>
+      <el-checkbox-pro key="6" value="6" indeterminate disabled>不确定-禁用</el-checkbox-pro>
+    </div>
+  </div>
 </template>
-<script>
-import createStoriesPreview from '@lcap/builder/input/vue2/stories-preview';
-import * as stories from '../../../components/el-checkbox-pro/stories/block.stories';
-
-const DemoPreview = createStoriesPreview(stories);
-
-export default {
-  components: {
-    DemoPreview,
-  },
-};
-
-</script>
