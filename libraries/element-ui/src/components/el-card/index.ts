b4af4add265bcde08548655e23dcb6cfc7ed58cc
sr2318522 <512828461@qq.com>
2024-09-24 14:34:45 +0800

    feat: add card style

--- libraries/element-ui/src/components/el-card/index.ts
+++ libraries/element-ui/src/components/el-card/index.ts
@@ -2,6 +2,7 @@ import Card from 'element-ui/lib/card';
 
 import { registerComponent } from '@lcap/vue2-utils/plugins/index';
 import * as plugins from './plugins';
+import './index.less';
 
 export const ElCard = registerComponent(Card, plugins, {
   nativeEvents: [],
