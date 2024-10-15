56e6bbbe06cf1250d9d969276f44fb0877ecd35e
AI Engineer <ai-engineer@163.com>
2024-10-15 20:43:07 +0800

    feat: 添加中文描述完整版

--- ae.css-selectors-config.js
+++ ae.css-selectors-config.js
@@ -2,15 +2,18 @@ const fs = require('fs-extra');
 
 module.exports = {
   mode: 'file-rpa',
-  prompt: fs.readFileSync('./ae.css-selectors-prompt.md'),
+  prompt: fs.readFileSync('./ae.css-selectors-prompt.md', 'utf-8'),
   includes: ['libraries/pc-ui/index.css-rules-desc.json'],
   excludes: [],
-  matchLine: (line) => /^\s*".+?":\s*""/.test(line),
-  postMatchLine: (line) => /^\s*".+?":\s*".+?"/.test(line),
-  postprocessLine(line, prevLine, nextLine) {
-    if (nextLine.trim()[0] === '}')
-      return line.replace(/,\s*$/, '');
-    else
-      return line;
-  }
+  linePattern: /^(\s*")(.+?)(":\s*")(.*?)(",?\s*)$/,
+  linePreFilter(line) {
+    const cap = line.match(this.linePattern);
+    return cap && !cap[4]; // 节省 token，只处理""的值
+  },
+  linePostProcess(line, oldLine) {
+    const cap = line.match(this.linePattern);
+    if (!cap) console.log('[linePostProcess] cap is null:', line);
+    if (!oldLine) console.log('[linePostProcess] oldLine is null:', line, oldLine);
+    return oldLine.replace(this.linePattern, (_, $1, $2, $3, $4, $5) => $1 + cap[2] + $3 + cap[4] + $5);
+  },
 }
