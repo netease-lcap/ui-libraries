ecbbca856df1159d293eaae8bcad38c63c123ab8
刘宇阳 <myronliu347@gmail.com>
2024-10-31 15:09:50 +0800

    fix(USelect): 单选选中值完整数据的展示

--- libraries/pc-ui/src/components/u-select.vue/index.vue
+++ libraries/pc-ui/src/components/u-select.vue/index.vue
@@ -79,7 +79,7 @@
                 selectedVM &&
                 (selectedVM.$slots && selectedVM.$slots.default
                   ? selectedVM.$slots.default
-                  : [$at2(selectedVM, field || textField)])
+                  : [$at2(selectedVM, field || textField) || currentText])
               "></f-render>
           </slot>
         </template>
