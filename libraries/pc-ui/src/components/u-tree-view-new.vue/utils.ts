a2b5c324369a6a6bc9cd0d0900d230088e1e8fe4
刘宇阳 <myronliu347@gmail.com>
2024-12-03 19:45:51 +0800

    style: code style

--- libraries/pc-ui/src/components/u-tree-view-new.vue/utils.ts
+++ libraries/pc-ui/src/components/u-tree-view-new.vue/utils.ts
@@ -1,7 +1,10 @@
-
 import { get as _get } from 'lodash';
 
-function forEachTreeData<T>(data: T[], { childrenField, parent }: { childrenField: string; parent?: T }, callback: (item: T, parent: T | undefined) => void) {
+function forEachTreeData<T>(
+  data: T[],
+  { childrenField, parent }: { childrenField: string; parent?: T },
+  callback: (item: T, p: T | undefined) => void,
+) {
   data.forEach((item) => {
     const children = _get(item, childrenField);
     if (Array.isArray(children) && children.length > 0) {
@@ -12,7 +15,11 @@ function forEachTreeData<T>(data: T[], { childrenField, parent }: { childrenFiel
   });
 }
 
-export function addChildrenValues<T, V>(item: T, checkList: Set<V>, { childrenField, valueField, disabledField }: { childrenField: string, valueField: string; disabledField: string }) {
+export function addChildrenValues<T, V>(
+  item: T,
+  checkList: Set<V>,
+  { childrenField, valueField, disabledField }: { childrenField: string; valueField: string; disabledField: string },
+) {
   const children = _get(item, childrenField);
 
   if (!Array.isArray(children) || children.length === 0) {
@@ -32,7 +39,11 @@ export function addChildrenValues<T, V>(item: T, checkList: Set<V>, { childrenFi
   });
 }
 
-export function removeChildrenValues<T, V>(item: T, checkList: Set<V>, { childrenField, valueField, disabledField }: { childrenField: string, valueField: string; disabledField: string }) {
+export function removeChildrenValues<T, V>(
+  item: T,
+  checkList: Set<V>,
+  { childrenField, valueField, disabledField }: { childrenField: string; valueField: string; disabledField: string },
+) {
   const children = _get(item, childrenField);
 
   if (!Array.isArray(children) || children.length === 0) {
@@ -46,32 +57,57 @@ export function removeChildrenValues<T, V>(item: T, checkList: Set<V>, { childre
     }
 
     const value = _get(it, valueField);
-    checkList.delete(value)
+    checkList.delete(value);
 
     removeChildrenValues(it, checkList, { childrenField, valueField, disabledField });
   });
 }
 
-export function addParentValues<T, V>(parent: T | undefined, { dataMap, childrenField, valueField, disabledField }: { dataMap: Record<any, undefined | { item: T, parent: T}>; childrenField: string; valueField: string; disabledField: string }, checkList: Set<V>, halfCheckList: Set<V>) {
+export function addParentValues<T, V>(
+  parent: T | undefined,
+  {
+    dataMap,
+    childrenField,
+    valueField,
+    disabledField,
+  }: {
+    dataMap: Record<any, undefined | { item: T; parent: T }>;
+    childrenField: string;
+    valueField: string;
+    disabledField: string;
+  },
+  checkList: Set<V>,
+  halfCheckList: Set<V>,
+) {
   if (!parent) {
     return;
   }
 
   const children = _get(parent, childrenField) as T[];
   const val = _get(parent, valueField) as V;
-  const checked = children.every((it) => (_get(it, disabledField) || checkList.has(_get(it, valueField))));
+  const checked = children.every((it) => _get(it, disabledField) || checkList.has(_get(it, valueField)));
 
   let pos = dataMap[val]?.parent;
 
   if (checked) {
     checkList.add(val);
-    addParentValues(pos, { dataMap, childrenField, valueField, disabledField }, checkList, halfCheckList);
+    addParentValues(
+      pos,
+      {
+        dataMap,
+        childrenField,
+        valueField,
+        disabledField,
+      },
+      checkList,
+      halfCheckList,
+    );
     return;
   }
 
   halfCheckList.add(val);
 
-  while(true) {
+  while (true) {
     if (!pos) {
       break;
     }
@@ -85,7 +121,7 @@ export function addParentValues<T, V>(parent: T | undefined, { dataMap, children
 
 const dataWeekMap = new WeakMap();
 export function getDataMap<T, V extends string | number>(data: T[], { childrenField, valueField }) {
-  let dataMap: Record<V, undefined | { item: T, parent: T}> = {} as any;
+  let dataMap: Record<V, undefined | { item: T; parent: T }> = {} as any;
   if (dataWeekMap.has(data)) {
     dataMap = dataWeekMap.get(data);
   } else {
@@ -102,27 +138,16 @@ export function getDataMap<T, V extends string | number>(data: T[], { childrenFi
   return dataMap;
 }
 
-export function filterParentValues<T, V extends string | number>(data: T[], values: V[],  { valueField, childrenField, disabledField }) {
-  if (!data || data.length === 0) {
-    return values;
-  }
-  const dataMap: Record<V, undefined | { item: T, parent: T}> = getDataMap(data, { valueField, childrenField });
-
-  return values.filter((v) => {
-    if (!dataMap[v] || !dataMap[v].item || isDisabled(data, v, { valueField, childrenField, disabledField })) {
-      return true;
-    }
-
-    return !(Array.isArray((dataMap[v].item as any).children) && (dataMap[v].item as any).children.length > 0);
-  });
-};
-
-export function isDisabled<T, V extends string | number>(data: T[], v: V, {  valueField, childrenField, disabledField }) {
+export function isDisabled<T, V extends string | number>(
+  data: T[],
+  v: V,
+  { valueField, childrenField, disabledField },
+) {
   let disabled = false;
-  const dataMap: Record<V, undefined | { item: T, parent: T}> = getDataMap(data, { valueField, childrenField });
+  const dataMap: Record<V, undefined | { item: T; parent: T }> = getDataMap(data, { valueField, childrenField });
   const { item } = dataMap[v] || {};
   let curItem = item;
-  while(!disabled) {
+  while (!disabled) {
     if (!curItem) {
       break;
     }
@@ -134,7 +159,30 @@ export function isDisabled<T, V extends string | number>(data: T[], v: V, {  val
   return disabled;
 }
 
-export function getCheckInfo<T, V extends string | number>(data: T[], values: V[], { valueField, childrenField, disabledField }) {
+export function filterParentValues<T, V extends string | number>(
+  data: T[],
+  values: V[],
+  { valueField, childrenField, disabledField },
+) {
+  if (!data || data.length === 0) {
+    return values;
+  }
+  const dataMap: Record<V, undefined | { item: T; parent: T }> = getDataMap(data, { valueField, childrenField });
+
+  return values.filter((v) => {
+    if (!dataMap[v] || !dataMap[v].item || isDisabled(data, v, { valueField, childrenField, disabledField })) {
+      return true;
+    }
+
+    return !(Array.isArray((dataMap[v].item as any).children) && (dataMap[v].item as any).children.length > 0);
+  });
+}
+
+export function getCheckInfo<T, V extends string | number>(
+  data: T[],
+  values: V[],
+  { valueField, childrenField, disabledField },
+) {
   if (!data || data.length === 0) {
     return {
       checkList: [...values],
@@ -142,7 +190,7 @@ export function getCheckInfo<T, V extends string | number>(data: T[], values: V[
       halfCheckList: [],
     };
   }
-  const dataMap: Record<V, undefined | { item: T, parent: T}> = getDataMap(data, { valueField, childrenField });
+  const dataMap: Record<V, undefined | { item: T; parent: T }> = getDataMap(data, { valueField, childrenField });
 
   const checkList = new Set<V>();
   const halfCheckList = new Set<V>();
@@ -165,7 +213,17 @@ export function getCheckInfo<T, V extends string | number>(data: T[], values: V[
     }
 
     addChildrenValues(item, checkList, { childrenField, valueField, disabledField });
-    addParentValues(parent, { dataMap, childrenField, valueField, disabledField }, checkList, halfCheckList);
+    addParentValues(
+      parent,
+      {
+        dataMap,
+        childrenField,
+        valueField,
+        disabledField,
+      },
+      checkList,
+      halfCheckList,
+    );
   });
 
   const list = [...checkList];
@@ -173,5 +231,5 @@ export function getCheckInfo<T, V extends string | number>(data: T[], values: V[
     checkedList: list,
     checkedValues: filterParentValues(data, list, { valueField, childrenField, disabledField }),
     halfCheckList: [...halfCheckList].filter((v) => !checkList.has(v)),
-  }
+  };
 }
