2d017af1b5cd5d16ae30889e8eba0f9caf235aa5
violarong <hzzhxr@corp.netease.com>
2025-01-06 19:53:10 +0800

    feat(ElToolTip): 添加IDE插件，点击展开tooltip

--- libraries/element-ui/src/components/el-tooltip/plugins/basic-plugins.ts
+++ libraries/element-ui/src/components/el-tooltip/plugins/basic-plugins.ts
@@ -1,2 +1,28 @@
-/* 组件功能扩展插件 */
-export {};
+import _, { isFunction } from 'lodash';
+import { VNode } from 'vue';
+import cls from 'classnames';
+
+function getCssRuleClassName(vnode: VNode, popperClass: string) {
+  if (!vnode) {
+    return popperClass;
+  }
+  const clx = cls(vnode.data?.class || [], vnode.data?.staticClass || '');
+  const cssRuleClassName = clx?.split(' ')?.find((name) => /^cw-css-rule-?/.test(name)) || '';
+
+  return `${popperClass} ${cssRuleClassName}`;
+}
+
+export const usePopperClass = {
+  setup(props, ctx) {
+    return {
+      popperClass: props.useComputed('popperClass', (popperClass = '') => {
+        const cssRuleClassName = getCssRuleClassName(
+          isFunction(ctx.getVNode) ? ctx.getVNode() : ctx.setupContext?.parent?.$vnode,
+          popperClass,
+        );
+
+        return cssRuleClassName;
+      }),
+    };
+  },
+};
