fdff3ba6954054c09cb1bf5961917527122f13df
LeeDan1124 <924448144@qq.com>
2025-01-10 14:01:51 +0800

    feat: 弹出消息&弹窗在preview中平铺展示

--- libraries/mobile-ui/src-vusion/theme/components/van-toast/index.vue
+++ libraries/mobile-ui/src-vusion/theme/components/van-toast/index.vue
@@ -1,16 +1,38 @@
 <template>
-<van-button @click="onOpen">弹出消息</van-button>
+  <div style="position: relative; height: 50px" ref="toastWrap">
+    <van-toast 
+      ref="toast"
+      message="弹出消息"
+      :duration="0" 
+      type="custom" 
+      @open="onShow" 
+      @close="onHide"></van-toast>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-     };
+  mounted() {
+    this.onOpen()
+    window.LcapMicro = {
+      appendTo: this.$refs.toastWrap
+    }
+  },
+  methods: {
+    onOpen() {
+      this.$refs.toast.open()
     },
-    methods: {
-        onOpen() {
-            this.$toast.show("弹出消息");
-        },
+    onShow() {
+      console.log('show1');
     },
+    onHide() {
+      console.log('hide1');
+    }
+  },
 };
 </script>
+
+<style>
+.van-toast-group {
+  position: absolute !important;
+}
+</style>
--- libraries/pc-ui/src/theme/components/u-modal/index.vue
+++ libraries/pc-ui/src/theme/components/u-modal/index.vue
@@ -1,26 +1,18 @@
 <template>
-<div>
-        <u-modal :visible.sync="exampleModalvisible" title="标题">
+    <div>
+        <u-modal title="标题" visible static @before-close="handleClose">
             <div style="position:relative;width:100%;height:85px;background:#F2F3F5;text-align:center;">
                 <u-text style="position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);">对话框内容</u-text>
             </div>
         </u-modal>
-        <u-linear-layout>
-            <u-button @click="onOpen">Modal</u-button>
-        </u-linear-layout>
     </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            exampleModalvisible: false,
-        };
-    },
     methods: {
-        onOpen() {
-            this.exampleModalvisible = true;
-        },
+        handleClose(ok) {
+            return ok.preventDefault()
+        }
     },
 };
 </script>
