f15dedca3ccc961bc6d08b8450aeb49b0ff699d3
sr2318522 <512828461@qq.com>
2025-03-14 13:58:51 +0800

    feat: 更新组件和示例，增强功能和可用性
    - 在 el-input 组件的故事中添加 v-model 支持，提升交互体验
    - 在 el-pagination 组件的故事中实现页面更新功能
    - 在 hooks.ts 中添加可控值的支持，优化状态管理
    - 修复多个文件中的格式问题，提升代码可读性

--- libraries/element-plus/src/components/el-pagination/stories/example.stories.js
+++ libraries/element-plus/src/components/el-pagination/stories/example.stories.js
@@ -1,3 +1,4 @@
+import { ref } from 'vue';
 import Components from '../index';
 
 export default {
@@ -13,6 +14,25 @@ export default {
 export const Default = {
   name: '基础示例',
   render: () => ({
-    template: '<el-pagination-plus size="small" layout="prev, pager, next" :total="50" />',
+    setup() {
+      const value = ref(2);
+      const myref = ref(null);
+      const handleUpdatePageSize = (value) => {
+        // console.log(value, 'value');
+        myref.value.onChange(3);
+      };
+      return {
+        value,
+        handleUpdatePageSize,
+        myref,
+      };
+    },
+    template: `
+    <div>
+    {{value}}
+    <el-pagination-plus ref="myref" v-model:current-page="value" size="small" layout="prev, pager, next" :total="50" />
+    <button @click="handleUpdatePageSize">更新  </button>
+    </div>
+    `,
   }),
 };
