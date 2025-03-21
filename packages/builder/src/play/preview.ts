75702d50efb263baa8104a70543c44dcf56d904f
刘宇阳 <myronliu347@gmail.com>
2025-03-21 13:08:58 +0800

    chore(Builder): change preview url

--- packages/builder/src/play/preview.ts
+++ packages/builder/src/play/preview.ts
@@ -1,3 +1,4 @@
+import portfinder from 'portfinder';
 import { getExtensionProjectMeta, getLcapConfig } from '../utils/project';
 import logger from '../utils/logger';
 import { getComponentMetaInfos } from '../utils/lcap';
@@ -8,29 +9,31 @@ export async function startPreview(rootPath: string, https: boolean = false) {
   const config = await getLcapConfig();
   const meta = await getExtensionProjectMeta(rootPath);
   const componentList = await getComponentMetaInfos(rootPath, true);
+  const port = await portfinder.getPortPromise({
+    port: 3000,
+    stopPort: 3333,
+  });
 
+  const protocol = https ? 'https' : 'http';
   let platformURL = config.platformURL || 'https://csforkf.lcap.codewave-test.163yun.com/';
 
-  platformURL = (https ? 'https' : 'http') + platformURL.substring(platformURL.indexOf('://'));
+  platformURL = `${protocol}${platformURL.substring(platformURL.indexOf('://'))}`;
 
   const version = '3.13.2';
 
   const commands: string[] = [
-    'npx lcap-ide-server-proxy',
-    `--PLATFORM_URL=${platformURL.startsWith('https') ? platformURL.replace('https:', 'http:') : platformURL}`,
+    'npx lcap-ide-server-proxy localserver',
+    `--PLATFORM_URL=${platformURL}`,
     '--LIB_PATH=.',
-  ];
-
-  const port = 3000;
-  const params: string[] = [
-    `IDE_VERSION=${version}`,
-    `LIB_TYPE=${componentList[0].type === 'h5' ? 'h5' : 'pc'}`,
-    `LIB_FRAMEWORK_KIND=${meta.framework}`,
-    `LIB_SERVICE=http://localhost:${port}/`,
+    `--IDE_VERSION=${version}`,
+    `--PORT=${port}`,
+    `--LIB_TYPE=${componentList[0].type === 'h5' ? 'h5' : 'pc'}`,
+    `--LIB_FRAMEWORK_KIND=${meta.framework}`,
+    // `--LIB_SERVICE=http://localhost:${port}/`,
   ];
 
   try {
-    previewURL = `${platformURL.endsWith('/') ? platformURL : `${platformURL}/`}designer/uidev?appId=demo&branch=feature-uidev3142&${params.join('&')}`;
+    previewURL = `${protocol}://localhost:${port}/designer/uidev?appId=demo&branch=feature-uidev3142&MODE=localserver&LIB_SERVICE=${protocol}://localhost:${port}`;
     logger.info(`ide 预览服务启动中...., 平台地址：${platformURL}`);
     await exec(commands.join(' '));
   } catch (e) {
