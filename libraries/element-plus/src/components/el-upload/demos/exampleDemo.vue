50bf1cd76846f7b7950d5b81d4bdc62e5354185e
wangxue08 <wangxue08@corp.netease.com>
2025-03-28 17:37:39 +0800

    fix(el-upload): set default action URL to '/upload'

--- libraries/element-plus/src/components/el-upload/demos/exampleDemo.vue
+++ libraries/element-plus/src/components/el-upload/demos/exampleDemo.vue
@@ -1,23 +1,17 @@
 <template>
-  <el-upload
-    v-model:file-list="fileList"
-    list-type="picture-card"
-    class="upload-demo"
-    action="/upload"
-    url-field="filePath"
-    multiple
-    :on-preview="handlePreview"
-    :on-remove="handleRemove"
-    :before-remove="beforeRemove"
-    :on-exceed="handleExceed"
-  >
-    <el-button type="primary">Click to upload</el-button>
-    <template #tip>
-      <div class="el-upload__tip">
-        jpg/png files with a size less than 500KB.
-      </div>
-    </template>
-  </el-upload>
+  <div>
+    <el-upload class="upload-demo" action="/upload"
+      url-field="filePath" multiple :on-preview="handlePreview" :on-remove="handleRemove" :before-remove="beforeRemove"
+    >
+      <el-button type="primary">Click to upload</el-button>
+      <template #tip>
+        <div class="el-upload__tip">
+          jpg/png files with a size less than 500KB.
+        </div>
+      </template>
+    </el-upload>
+    <!-- <el-text :text="fileList"></el-text> -->
+  </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue'
@@ -25,16 +19,7 @@ import { ElMessage, ElMessageBox } from 'element-plus'
 
 import type { UploadProps, UploadUserFile } from 'element-plus'
 
-const fileList = ref<UploadUserFile[]>([
-  {
-    name: 'element-plus-logo.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-  {
-    name: 'element-plus-logo2.svg',
-    url: 'https://element-plus.org/images/element-plus-logo.svg',
-  },
-])
+const fileList = ref<String>('https://element-plus.org/images/element-plus-logo.svg,https://element-plus.org/images/element-plus-logo.svg')
 
 const handleRemove: UploadProps['onRemove'] = (file, uploadFiles) => {
   console.log(file, uploadFiles)
