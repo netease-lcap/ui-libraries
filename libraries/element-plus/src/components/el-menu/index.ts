2566ef3a3fb3d415c31659d50d6249b290cb3d62
wangxue08 <wangxue08@corp.netease.com>
2025-03-28 11:18:02 +0800

    fix(el-menu): 修复菜单链接无法跳转

--- libraries/element-plus/src/components/el-menu/index.ts
+++ libraries/element-plus/src/components/el-menu/index.ts
@@ -2,6 +2,7 @@ import { ElMenu as ElMenuPlus, ElMenuItem as ElMenuItemPlus, ElMenuItemGroup as 
 import { registerComponent } from '@/plugins';
 import * as basicPlugin from './plugins/basic-plugins';
 import * as itemPlugin from './plugins/item-plugin';
+import * as menuItemPlugin from './plugins/menu-item';
 
 import 'element-plus/theme-chalk/el-menu.css';
 import 'element-plus/theme-chalk/el-menu-item.css';
@@ -10,7 +11,7 @@ import 'element-plus/theme-chalk/el-sub-menu.css';
 import './index.css';
 
 const ElMenu = registerComponent(ElMenuPlus, { plugin: basicPlugin });
-const ElMenuItem = registerComponent(ElMenuItemPlus, { plugin: itemPlugin });
+const ElMenuItem = registerComponent(ElMenuItemPlus, { plugin: menuItemPlugin });
 const ElMenuItemGroup = registerComponent(ElMenuItemGroupPlus, { plugin: itemPlugin });
 const ElSubMenu = registerComponent(ElSubMenuPlus, { plugin: itemPlugin });
 
--- libraries/element-plus/src/components/el-menu/plugins/basic-plugins.tsx
+++ libraries/element-plus/src/components/el-menu/plugins/basic-plugins.tsx
@@ -82,28 +82,3 @@ export const handleSlotDefault = (props) => {
     },
   };
 };
-
-export function handleHrefToRouter(props) {
-  const destination = props.get('destination');
-  const link = props.get('link');
-  const href = props.get('href');
-  const target = props.get('target');
-  const onClick = props.get('onClick');
-  const router = props.get('$router');
-  const destinationToRouterClick = _.cond([
-    [_.matches({ target: '_blank' }), _.constant(() => {})],
-    [_.isString, (params) => () => router.push(params.destination)],
-    [_.stubTrue, _.constant(() => {})],
-  ]);
-  const routerClick = destinationToRouterClick({ destination, target });
-  const isHref = !_.isNil(link) || !_.isNil(href);
-  const hrefObject = isHref ? { href: link || href, target } : {};
-
-  return {
-    onClick: _.wrap(onClick, (fn, ...args) => {
-      _.attempt(fn, ...args);
-      _.attempt(routerClick, ...args);
-    }),
-    ...hrefObject,
-  };
-}
