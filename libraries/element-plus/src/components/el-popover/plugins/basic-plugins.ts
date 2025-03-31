b7d54ae999e789fe7968cc0eaa280ae13c8a717c
LeeDan1124 <924448144@qq.com>
2025-03-31 15:18:51 +0800

    fix(ElPopper): 透传class到popper上

--- libraries/element-plus/src/components/el-popover/plugins/basic-plugins.ts
+++ libraries/element-plus/src/components/el-popover/plugins/basic-plugins.ts
@@ -1,1 +1,8 @@
-export {};
+export function handlePopperClass(props) {
+  const popperClassProp = props.get('popperClass');
+  const setClass = props.get('class');
+
+  return {
+    popperClass: `${popperClassProp} ${setClass}`,
+  };
+}
