a5c0d9962d6031e476d301e16f393c1b075b2ae5
不可 <512828461@qq.com>
2025-04-01 17:47:41 +0800

    feat(el-link): 使用getPropsIcon函数替代ElIcon，优化图标属性获取逻辑

--- libraries/element-plus/src/components/el-link/plugins/index.tsx
+++ libraries/element-plus/src/components/el-link/plugins/index.tsx
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import { $deletePropsList } from '@/plugins/constants';
-import { ElIcon } from '@/components';
+import { getPropsIcon } from '@/plugins/common/icon';
 
 export * from './ide.ts';
 
@@ -14,7 +14,7 @@ export function handleTextToSlots(props) {
       default: () => text,
     }),
     [$deletePropsList]: deletePropsList,
-    icon: <ElIcon name={icon} />,
+    icon: getPropsIcon({ name: icon }),
   };
 }
 
