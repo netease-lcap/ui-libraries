907ad242704baa293cb226dbb58ec208aef9d1b3
不可 <512828461@qq.com>
2025-04-01 16:22:09 +0800

    feat(el-mention, el-transfer): 添加与表单项的关联，更新el-transfer组件的表单项名称以保持一致性

--- libraries/element-plus/src/components/el-mention/index.ts
+++ libraries/element-plus/src/components/el-mention/index.ts
@@ -2,8 +2,11 @@
 import { ElMention as ElMentionPlus } from 'element-plus';
 import { registerComponent } from '@/plugins';
 import * as basicPlugin from './plugins/index';
+import { withFormItem } from '@/components/el-form/plugins/form-item';
 
 export const ElMention = registerComponent(ElMentionPlus, {
   plugin: basicPlugin,
 });
+
+export const ElFormMention = withFormItem(ElMention, 'el-form-mention');
 export default ElMentionPlus;
