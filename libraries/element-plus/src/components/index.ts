527b99f3ec16a80acb07df2ef7c3a56224b2d44e
不可 <512828461@qq.com>
2025-04-01 16:23:55 +0800

    fix(el-transfer): 更新el-transfer组件的表单项名称以保持一致性

--- libraries/element-plus/src/components/index.ts
+++ libraries/element-plus/src/components/index.ts
@@ -47,7 +47,7 @@ export { ElMessageBox, ElMessageBoxDesigner } from './el-message-box';
 export { ElNotification, ElNotificationDesigner } from './el-notification';
 export { ElLoading } from './el-loading';
 export { ElDatePicker, ElFormDatePicker } from './el-date-picker';
-export { ElTransfer, ElFormElTransfer } from './el-transfer';
+export { ElTransfer, ElFormTransfer } from './el-transfer';
 export { ElTree } from './el-tree';
 export { ElTooltip } from './el-tooltip';
 export { ElProgress } from './el-progress';
