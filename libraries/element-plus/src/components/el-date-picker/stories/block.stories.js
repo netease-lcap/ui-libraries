789bbd8560af601943ea4932a72f9c373e5dd0d0
zhuanghengheng <zhuanghengheng@corp.netease.com>
2025-04-02 11:00:01 +0800

    fix: 修改多个组件

--- libraries/element-plus/src/components/el-date-picker/stories/block.stories.js
+++ libraries/element-plus/src/components/el-date-picker/stories/block.stories.js
@@ -23,13 +23,13 @@ export default {
 export const Date = {
   name: '日期选择器',
   render: () => ({
-    template: '<el-date-picker type="date"></el-date-picker>',
+    template: '<el-date-picker type="date" placeholder="请选择日期"></el-date-picker>',
   }),
 };
 
-// export const DateTime = {
-//   name: '日期时间选择器',
-//   render: () => ({
-//     template: '<el-date-picker type="datetime"></el-date-picker>',
-//   }),
-// };
+export const DateTime = {
+  name: '日期时间选择器',
+  render: () => ({
+    template: '<el-date-picker type="datetime" placeholder="请选择时间"></el-date-picker>',
+  }),
+};
