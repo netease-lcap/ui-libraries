d16a19697bb7fea1be29827cc1b13cf21ec6042c
刘宇阳 <myronliu347@gmail.com>
2025-04-14 19:57:24 +0800

    fix(ElementUI): 修复树形结构动态添加时数据重复问题

--- libraries/element-ui/src/pro-components/el-tree-select-pro/plugins/basic-plugins.ts
+++ libraries/element-ui/src/pro-components/el-tree-select-pro/plugins/basic-plugins.ts
@@ -1,5 +1,5 @@
 import _, { isFunction, get as lodashGet } from 'lodash';
-
+import { listToTree } from '@lcap/vue2-utils/utils';
 import { createUseUpdateSync } from '@lcap/vue2-utils';
 import { computed } from '@vue/composition-api';
 import { NaslComponentPluginOptions, Slot } from '@lcap/vue2-utils/plugins/types';
@@ -11,27 +11,8 @@ export { usePopupTheme } from '../../../plugins/use-popup-theme';
 
 export const useUpdateSync = createUseUpdateSync();
 
-function listToTree(dataSource, parentField, valueField = 'value') {
-  if (_.isNil(parentField)) return dataSource;
-  const map = new Map<string, Record<string, any>>(
-    dataSource.map((item) => [_.get(item, valueField), item]),
-  );
-  const tree = [] as any[];
-  dataSource.forEach((item) => {
-    if (map.get(_.get(item, parentField))) {
-      const parent = map.get(_.get(item, parentField));
-      if (!parent) return;
-      if (!Array.isArray(parent.children)) parent.children = [];
-      parent.children.push(map.get(_.get(item, valueField)));
-    } else {
-      tree.push(map.get(_.get(item, valueField)));
-    }
-  });
-  return tree;
-}
-
 export const useTreeSelect: NaslComponentPluginOptions = {
-  props: ['valueField', 'labelField', 'data', 'optionIsSlot'],
+  props: ['valueField', 'labelField', 'parentField', 'data', 'optionIsSlot'],
   setup(props, ctx) {
     const valueField = props.useComputed('valueField', (v) => v || 'value');
     const textField = props.useComputed('textField', (v) => v || 'label');
@@ -44,7 +25,11 @@ export const useTreeSelect: NaslComponentPluginOptions = {
     const data = props.useComputed('data', (dataSource) => {
       if (_.isEmpty(dataSource)) return undefined;
       if (_.isNil(parentField.value)) return dataSource;
-      return listToTree(dataSource, parentField.value, valueField.value);
+      return listToTree(dataSource, {
+        valueField: valueField.value,
+        parentField: parentField.value,
+        childrenField: childrenField.value,
+      });
     });
     const keys = props.useComputed('keys', (v) => (_.isObject(v) ? v : {}));
 
