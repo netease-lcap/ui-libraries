f66b7a83ea5f06b29fa97b9fdc3b03998dd0907f
不可 <512828461@qq.com>
2025-04-14 15:38:56 +0800

    feat: add setter

--- setters/src/setters/NormalSetter.vue
+++ setters/src/setters/NormalSetter.vue
@@ -1,35 +1,39 @@
 <template>
-  <input :value="value" :class="$style.input" @input="handleInput" />
-  </template>
-  <script setup>
-  
-  const props = defineProps({
-    value: {
-      default: '',
-    },
-    onChange: {
-      default: () => () => {},
-    }
-  });
-  
-  const handleInput = (e) => {
-    props.onChange(e.target.value)
+  <el-button @click="dialogFormVisible = true">按钮</el-button>
+  <el-dialog v-model="dialogFormVisible" title="Shipping address" width="500">
+    <el-input v-model="myValue" @input="handleInput" autocomplete="off" />
+  </el-dialog>
+</template>
+<script setup>
+import { ref } from 'vue';
+const props = defineProps({
+  value: {
+    default: '',
+  },
+  onChange: {
+    default: () => () => {},
+  },
+});
+
+const dialogFormVisible = ref(false);
+const myValue = ref('');
+const handleInput = (e) => {
+  props.onChange(e);
+};
+</script>
+<style module>
+.input {
+  width: 256px;
+  height: 28px;
+  background-color: #fff;
+  outline: none;
+  border: 1px solid #ccc;
+  color: #333;
+
+  border-radius: 4px;
+  &:hover,
+  &:focus {
+    border-color: blue;
   }
-  
-  </script>
-  <style module>
-  .input {
-    width: 256px;
-    height: 28px;
-    background-color: #fff;
-    outline: none;
-    border: 1px solid #ccc;
-    color: #333;
-  
-    border-radius: 4px;
-    &:hover,
-    &:focus {
-      border-color: blue;
-    }
-  }
-  </style>
+}
+</style>
