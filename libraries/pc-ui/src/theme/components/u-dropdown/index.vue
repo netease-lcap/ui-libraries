ec300daccb28b81339411e1f3f08222dad555160
刘宇阳 <myronliu347@gmail.com>
2025-07-04 15:15:11 +0800

    feat(UDropdown): 添加图标设置

--- libraries/pc-ui/src/theme/components/u-dropdown/index.vue
+++ libraries/pc-ui/src/theme/components/u-dropdown/index.vue
@@ -1,3 +1,57 @@
 <template>
-<u-dropdown><template #title="scope"><u-text text="下拉菜单"></u-text></template><u-dropdown-item text="item"></u-dropdown-item><u-dropdown-item text="item"></u-dropdown-item><u-dropdown-item text="item"></u-dropdown-item><u-dropdown-item disabled text="item"></u-dropdown-item></u-dropdown>
+  <u-linear-layout>
+    <u-dropdown>
+      <template #title="scope"> <u-text text="下拉菜单"></u-text></template>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-group title="下拉组">
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item disabled text="item1"></u-dropdown-item>
+      </u-dropdown-group>
+    </u-dropdown>
+    <u-dropdown type="primary">
+      <template #title="scope"> <u-text text="下拉菜单"></u-text></template>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-group title="下拉组">
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item disabled text="item1"></u-dropdown-item>
+      </u-dropdown-group>
+    </u-dropdown>
+    <u-dropdown type="primary_secondary">
+      <template #title="scope"> <u-text text="下拉菜单"></u-text></template>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-group title="下拉组">
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item disabled text="item1"></u-dropdown-item>
+      </u-dropdown-group>
+    </u-dropdown>
+    <u-dropdown type="normal">
+      <template #title="scope"> <u-text text="下拉菜单"></u-text></template>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-group title="下拉组">
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item disabled text="item1"></u-dropdown-item>
+      </u-dropdown-group>
+    </u-dropdown>
+    <u-dropdown type="more">
+      <template #title="scope"> <u-text text="下拉菜单"></u-text></template>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-item text="item"> </u-dropdown-item>
+      <u-dropdown-group title="下拉组">
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item text="item1"></u-dropdown-item>
+        <u-dropdown-item disabled text="item1"></u-dropdown-item>
+      </u-dropdown-group>
+    </u-dropdown>
+  </u-linear-layout>
 </template>
