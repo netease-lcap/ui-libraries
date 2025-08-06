17969e8c42da5c71124e42ab2dc11546070bf618
LeeDan1124 <924448144@qq.com>
2025-08-06 20:01:52 +0800

    fix: vant第一轮测试bug修复

--- libraries/vant/src/components/van-swipe/plugins/index.tsx
+++ libraries/vant/src/components/van-swipe/plugins/index.tsx
@@ -1,8 +1,9 @@
 /* 组件功能扩展插件 */
 import _ from 'lodash';
 import { $deletePropsList, $dataSourceDeleteField } from '@/plugins/constants';
-import { useRequestDataSource, useHandleMapField, useFormatDataSource } from '@/plugins/common/dataSource';
+import { useRequestDataSource, useFormatDataSource } from '@/plugins/common/dataSource';
 import { useMemo } from '@/plugins/hooks';
+import { VanSwipeItem } from '../index';
 
 export function handleDataSource(props) {
   const dataConfig = props.get('dataSource');
@@ -11,15 +12,15 @@ export function handleDataSource(props) {
   const deletePropsList = props.get($deletePropsList).concat($dataSourceDeleteField);
   const ref = props.get('ref');
   const { data, run: reload, loading } = useRequestDataSource(dataConfig);
-  const dataSource = useHandleMapField({ dataSource: useFormatDataSource(data) });
+  const dataSource = useFormatDataSource(data);
   const selfRef = useMemo(() => _.assign(ref, { reload, data: dataSource }), [dataSource, reload, ref]);
 
   const dataSourceSlots = useMemo(() => (_.isNil(dataConfig)
       ? {}
       : {
           default: () => _.map(dataSource, (item) => (
-            <van-swipe-item
-              {...item}
+            <VanSwipeItem
+              // {...item}
               v-slots={{
                 default: () => slots.content?.({ item }),
               }}
