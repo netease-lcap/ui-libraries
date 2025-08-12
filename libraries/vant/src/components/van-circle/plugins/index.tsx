8390b71fc894a3af7eb0371b3d8d62845dbab618
= <512828461@qq.com>
2025-08-12 17:14:46 +0800

    feat(vant): 添加 handleText 函数以处理文本属性，并更新示例中的 currentRate 值

--- libraries/vant/src/components/van-circle/plugins/index.tsx
+++ libraries/vant/src/components/van-circle/plugins/index.tsx
@@ -1,1 +1,8 @@
-export {};
+export function handleText(props) {
+  const textProps = props.get('text');
+  const currentRate = props.get('currentRate');
+  const text = textProps || `${currentRate}%`;
+  return {
+    text,
+  };
+}
