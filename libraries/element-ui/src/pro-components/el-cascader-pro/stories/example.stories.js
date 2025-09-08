6dd00fe4289e1d7dbe03235f3fa47c3142a98907
taoyifeng <taoyifeng@corp.netease.com>
2025-09-08 10:36:35 +0800

    fix: cascader remove event

--- libraries/element-ui/src/pro-components/el-cascader-pro/stories/example.stories.js
+++ libraries/element-ui/src/pro-components/el-cascader-pro/stories/example.stories.js
@@ -67,3 +67,70 @@ export const Default = {
     ></el-cascader-pro>`,
   }),
 };
+
+export const Multi = {
+  name: '多选示例',
+  render: () => ({
+    data() {
+      return {
+        data: [
+          {
+            label: '选项一',
+            value: '1',
+            children: [
+              {
+                label: '子选项一',
+                value: '1.1',
+              },
+              {
+                label: '子选项二',
+                value: '1.2',
+              },
+              {
+                label: '子选项三',
+                value: '1.3',
+              },
+            ],
+          },
+          {
+            label: '选项二',
+            value: '2',
+            children: [
+              {
+                label: '子选项一',
+                value: '2.1',
+              },
+              {
+                label: '子选项二',
+                value: '2.2',
+              },
+            ],
+          },
+        ],
+        list: [
+          { id: 1, name: 'Node 1', parentId: null },
+          { id: 2, name: 'Node 1.1', parentId: 1 },
+          { id: 3, name: 'Node 1.1.1', parentId: 2 },
+          { id: 4, name: 'Node 1.2', parentId: 1 },
+          { id: 5, name: 'Node 2', parentId: null },
+          { id: 6, name: 'Node 2.1', parentId: 5 },
+        ],
+      };
+    },
+    methods: {
+      handleRemoveTag(checked, node) {
+        console.log('====>handleRemoveTag ', checked, node);
+      },
+    },
+    template: `<el-cascader-pro
+      :dataSource="list"
+      valueField="id"
+      multiple
+      clearable
+      textField="name"
+      parentField="parentId"
+      @remove="handleRemoveTag"
+    ></el-cascader-pro>`,
+  }),
+};
+
