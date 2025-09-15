7df43d2f8b1b6d3702c5a14b196538feffe9e917
= <512828461@qq.com>
2025-09-15 15:43:35 +0800

    refactor: 更新 el-badge 组件的插件导入路径，简化插件导入方式

--- libraries/element-plus/src/components/el-badge/index.ts
+++ libraries/element-plus/src/components/el-badge/index.ts
@@ -1,7 +1,7 @@
 import { ElBadge as ElBadgePlus } from 'element-plus';
 import _ from 'lodash';
 import { registerComponent } from '@/plugins';
-import * as basicsPlugin from './plugins/index';
+import basicsPlugin from './plugins/basic-plugins';
 
 function ElBadgeRegister(BaseComponent, plugin = {}, extend = true) {
   const componentPlugin = extend ? _.assign(basicsPlugin, plugin) : plugin;
