357f29b1e1d1c5cd912c395299c9c0f97b1d32ca
= <512828461@qq.com>
2025-09-16 10:03:19 +0800

    refactor: 更新 el-input 和 el-tree-select 组件的插件导入方式，统一使用大写组件名，优化预览文本渲染逻辑

--- libraries/element-plus/src/components/el-input/plugins/index.tsx
+++ libraries/element-plus/src/components/el-input/plugins/index.tsx
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import { getPropsIcon } from '@/plugins/common/icon';
 import { getIsPreview, getRender } from '@/plugins/common/preview';
+import { ElPreview } from '@/index';
 
 export { handleComponentInForm } from '@/components/el-form/plugins/form-item';
 export { handleControllableValue } from '@/plugins/common/index';
@@ -49,7 +50,7 @@ export function handlePreview(props) {
   const previewRender = (insProps) => {
     const inIDE = !!props.get('data-nodepath');
     const previewText = inIDE || _.isEmpty(insProps.modelValue) ? '-' : insProps.modelValue;
-    return <el-preview text={previewText} />;
+    return <ElPreview text={previewText} />;
   };
 
   const { render, insRef } = getRender(Component, previewRender, isPreview);
--- libraries/element-plus/src/components/el-tree-select/plugins/index.tsx
+++ libraries/element-plus/src/components/el-tree-select/plugins/index.tsx
@@ -9,6 +9,7 @@ import {
 import { $deletePropsList } from '@/plugins/constants';
 import { useMemo } from '@/plugins/hooks';
 import { getIsPreview, getRender, getTreePreviewText } from '@/plugins/common/preview';
+import { ElPreview } from '@/index';
 
 export { handleComponentInForm } from '@/components/el-form/plugins/form-item';
 export { handleControllableValue } from '@/plugins/common/index';
@@ -57,7 +58,7 @@ export function handlePreview(props) {
     const valueField = props.get('valueField', 'value');
     const value = getTreePreviewText(textField, valueField, insProps.data, _.compact(_.castArray(insProps.modelValue)));
     const previewText = inIDE ? '-' : value;
-    return <el-preview text={previewText} />;
+    return <ElPreview text={previewText} />;
   };
   const { render, insRef } = getRender(Component, previewRender, isPreview);
 
