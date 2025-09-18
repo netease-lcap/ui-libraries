a1e5113207fdb906f4fa0cd7179a69bb6424f3c0
= <512828461@qq.com>
2025-09-18 14:27:11 +0800

    feat: 为 ElIcon 组件添加 color 属性，并在渲染时传递 props

--- libraries/element-plus/src/components/el-icon/icon.tsx
+++ libraries/element-plus/src/components/el-icon/icon.tsx
@@ -12,6 +12,11 @@ export const ElIconPropsDefine = {
     type: Function,
     default: () => {},
   },
+  color: {
+    type: String,
+
+    default: () => '',
+  },
   size: {
     type: [String, Number],
     default: () => '',
@@ -83,7 +88,7 @@ export default defineComponent({
         }
       }
 
-      return <ElIconPlus>{iconComponent ? h(iconComponent) : null}</ElIconPlus>;
+      return <ElIconPlus>{iconComponent ? h(iconComponent, props) : null}</ElIconPlus>;
     }
     return () => {
       return !props.name ? null : renderChildren();
