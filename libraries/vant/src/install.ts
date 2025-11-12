908100a8c03b73b7ecbaf2fc96c06d7cd21b530d
御风 <18012261618@126.com>
2025-11-12 11:44:24 +0800

    chore(vant): 添加setupAppConfiguration 函数 (#1287)

--- libraries/vant/src/install.ts
+++ libraries/vant/src/install.ts
@@ -3,10 +3,12 @@ import { showToast } from 'vant';
 // import Vant from 'vant';
 import * as Components from './components';
 
-export const install: Plugin = (app) => {
-  Object.keys(Components).forEach((name) => {
-    app.component(name, Components[name]);
-  });
+/**
+ * Setup Vant app configuration
+ * without registering components.
+ * @param app
+ */
+export const setupAppConfiguration: Plugin = (app) => {
   app.config.globalProperties.$message = {
     info: (msg: string) => {
       showToast({
@@ -21,4 +23,12 @@ export const install: Plugin = (app) => {
       });
     },
   };
+};
+
+export const install: Plugin = (app) => {
+  setupAppConfiguration(app);
+
+  Object.keys(Components).forEach((name) => {
+    app.component(name, Components[name]);
+  });
 };
