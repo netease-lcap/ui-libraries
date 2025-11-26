622ca57724d843a1db5afe60a7decd7597ae567e
= <512828461@qq.com>
2025-11-26 11:09:10 +0800

    fix(el-collapse): 修改 slotTitle 和 slotIcon 参数名称为 current

--- libraries/element-plus/src/components/el-collapse/api.ts
+++ libraries/element-plus/src/components/el-collapse/api.ts
@@ -229,12 +229,12 @@ namespace nasl.ui {
       title: '标题',
       description: 'Collapse Item标题',
     })
-    slotTitle: (event: { isActive: nasl.core.Boolean }) => Array<ViewComponent>;
+    slotTitle: (current: { isActive: nasl.core.Boolean }) => Array<ViewComponent>;
 
     @Slot({
       title: '图标内容',
-      description: '图标内容',
+      description: 'Collapse Item图标内容',
     })
-    slotIcon: (event: { isActive: nasl.core.Boolean }) => Array<ViewComponent>;
+    slotIcon: (current: { isActive: nasl.core.Boolean }) => Array<ViewComponent>;
   }
 }
