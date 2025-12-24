b873e8ed77531e4a60292a6bfa334b2195df9cb3
= <512828461@qq.com>
2025-12-24 16:51:56 +0800

    refactor(van-dropdown-menu): 优化数据源处理逻辑，移除未使用的选项映射

--- libraries/vant/src/components/van-dropdown-menu/plugins/item.tsx
+++ libraries/vant/src/components/van-dropdown-menu/plugins/item.tsx
@@ -22,50 +22,55 @@ export function handleDataSource(props) {
 
   const { data, run: reload, loading } = useRequestDataSource(dataConfig, {});
   const dataSource = useHandleMapField({
+    label: 'text',
     textField,
     valueField,
     disabledField,
     // iconField,
     dataSource: useFormatDataSource(data),
+    fieldsMap: {
+      icon: iconField,
+    },
   });
   const selfRef = useMemo(() => _.assign(ref, { reload, data: dataSource }), [dataSource, reload, ref]);
+  // console.log(dataSource, 'dataSource===');
 
-  const options = useMemo(() => {
-    return _.map(dataSource, (item: any) => ({
-      text: item[textField],
-      value: item[valueField],
-      disabled: item[disabledField],
-      icon: item[iconField],
-    }));
-    }, [dataSource, textField, valueField, disabledField, iconField]);
+  // const options = useMemo(() => {
+  //   return _.map(dataSource, (item: any) => ({
+  //     text: item[textField],
+  //     value: item[valueField],
+  //     disabled: item[disabledField],
+  //     icon: item[iconField],
+  //   }));
+  // }, [dataSource, textField, valueField, disabledField, iconField]);
 
-    // console.log('👨‍👩‍👧‍👦', dataSource);
-    // const dataSourceSlots = useMemo(() => {
-    //   return _.isNil(dataConfig) ? {} : {
-    //     // TODO LD:需要传 disabled
-    //     default: () => _.map(dataSource, (item) => (
-    //       <van-dropdown-item-son
-    //         center
-    //         title={item[textField]}
-    //         disabled={item[disabledField]}
-    //         icon={item[iconField]}
-    //         titleStyle={{
-    //           color: value === item[valueField] ? activeColor : item[disabledField] ? '#999' : '',
-    //           cursor: item[disabledField] ? 'not-allowed' : 'pointer',
-    //         }}
-    //         onClick={() => {
-    //           if (item[disabledField]) return;
-    //           setValue(item[valueField]);
-    //           onChange?.(item[valueField]);
-    //           selfRef?.toggle?.();
-    //         }}
-    //         v-slots={{
-    //           'right-icon': value === item[valueField] ? () => <Icon name="success" color={activeColor} /> : null,
-    //         }}
-    //       />
-    //     )),
-    //   };
-    // }, [dataSource, textField, valueField, disabledField, iconField, value]);
+  // console.log('👨‍👩‍👧‍👦', dataSource);
+  // const dataSourceSlots = useMemo(() => {
+  //   return _.isNil(dataConfig) ? {} : {
+  //     // TODO LD:需要传 disabled
+  //     default: () => _.map(dataSource, (item) => (
+  //       <van-dropdown-item-son
+  //         center
+  //         title={item[textField]}
+  //         disabled={item[disabledField]}
+  //         icon={item[iconField]}
+  //         titleStyle={{
+  //           color: value === item[valueField] ? activeColor : item[disabledField] ? '#999' : '',
+  //           cursor: item[disabledField] ? 'not-allowed' : 'pointer',
+  //         }}
+  //         onClick={() => {
+  //           if (item[disabledField]) return;
+  //           setValue(item[valueField]);
+  //           onChange?.(item[valueField]);
+  //           selfRef?.toggle?.();
+  //         }}
+  //         v-slots={{
+  //           'right-icon': value === item[valueField] ? () => <Icon name="success" color={activeColor} /> : null,
+  //         }}
+  //       />
+  //     )),
+  //   };
+  // }, [dataSource, textField, valueField, disabledField, iconField, value]);
 
   return {
     // ...valueProps,
@@ -73,6 +78,6 @@ export function handleDataSource(props) {
     ref: selfRef,
     loading,
     data,
-    options,
+    options: dataSource,
   };
 }
