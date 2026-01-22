c184349899248cae78c264a5d19ca485bc0b0fcf
= <512828461@qq.com>
2026-01-22 16:13:21 +0800

    feat: 合并 4.4select 功能到 4.1

--- libraries/element-plus/src/components/el-select/plugins/index.tsx
+++ libraries/element-plus/src/components/el-select/plugins/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-shadow */
 import _ from 'lodash';
 import { ElSelectV2, SelectProps } from 'element-plus';
-import { useMemo, useCallback } from '@/plugins/hooks';
+import { CSSProperties } from 'vue';
+import { getPropsIcon } from '@/plugins/common/icon';
+import { useMemo, useCallback, useEffect } from '@/plugins/hooks';
 import { $deletePropsList, $dataSourceDeleteField } from '@/plugins/constants';
 import { useRequestDataSource, useHandleMapField, useFormatDataSource } from '@/plugins/common/dataSource';
 import { ElOption } from '../index';
@@ -11,6 +13,7 @@ import { PluginAccumulateTypes } from '@/plugins/accumulate';
 import { handleComponentInForm } from '@/components/el-form/plugins/form-item';
 import { handleControllableValue } from '@/plugins/common/index';
 import { IIdePluginBase } from '@/types';
+import { addClass } from '@/utils';
 
 const SelectBasicAccumulate = new PluginAccumulateTypes<
   nasl.ui.ElSelectOptions<any, any, any, any, any>,
@@ -39,25 +42,77 @@ export default SelectBasicAccumulate.addPlugin({
       const dataConfig = props.get('dataSource');
       const textField = props.get('textField') || 'label';
       const valueField = props.get('valueField') || 'value';
-      const slots = props.get('slots');
+      const descriptionField = props.get('descriptionField') || 'description';
+      const onSuccess = props.get('onSuccess', () => { });
       const deletePropsList = props.get($deletePropsList).concat($dataSourceDeleteField, ['formTagName'], 'data');
       const ref = props.get('ref');
-      const { data, run: reload, loading } = useRequestDataSource(dataConfig);
-      const dataSource = useHandleMapField({ textField, valueField, dataSource: useFormatDataSource(data) });
+
+      const {
+        data,
+        run: reload,
+        loading,
+      } = useRequestDataSource(dataConfig, {
+        onSuccess,
+      });
+      const dataSource = useHandleMapField({
+        textField,
+        valueField,
+        dataSource: useFormatDataSource(data),
+        fieldsMap: {
+          description: descriptionField,
+        },
+      });
       const selfRef = useMemo(() => _.assign(ref, { reload, data: dataSource }), [dataSource, reload, ref]);
-      const dataSourceSlots = _.isNil(dataConfig)
-        ? {}
-        : { default: () => _.map(dataSource, (item) => <ElOption {...item} />) };
 
       return {
         [$deletePropsList]: deletePropsList,
         ref: selfRef,
         loading,
-        slots: _.assign(slots, dataSourceSlots),
         data: dataSource,
       };
     },
   })
+  .addPlugin({
+    name: 'handleValueNotInData',
+    handle(props) {
+      const selectedValuesData = props.get('selectedValuesData');
+
+      const data = props.get('data', []);
+      if (_.isEmpty(selectedValuesData) || _.isEmpty(data)) return {};
+      return {
+        data: _.unionBy(data, selectedValuesData, 'value'),
+      };
+    },
+  })
+  .addPlugin({
+    name: 'handleSlotRender',
+    handle(props) {
+      const dataConfig = props.get('dataSource');
+      const slots = props.get('slots');
+      const data = props.get('data');
+      const optionSlot = props.get('optionSlot');
+      const dataSourceSlots = _.isNil(dataConfig)
+        ? {}
+        : {
+          default: () => _.map(data, (item, index) => (
+            <ElOption {...item}>
+              {optionSlot ? slots?.item?.({ index, item: item?.itemSource ?? item } as any) : item.label}
+              {item.description && (
+                <el-text
+                  style={{ display: 'block', height: '14px', lineHeight: '14px' } as CSSProperties}
+                  color="secondary"
+                  text={item.description}
+                />
+              )}
+            </ElOption>
+          )),
+        };
+      return {
+        slots: _.assign({}, slots, dataSourceSlots),
+      };
+    },
+  })
+
   .addPlugin({
     name: 'handleVirtualize',
     handle(props) {
@@ -68,10 +123,10 @@ export default SelectBasicAccumulate.addPlugin({
       const result = useMemo(() => {
         return virtualize
           ? {
-              options: data,
-              render,
-              slots: _.omit(slots, 'default'),
-            }
+            options: data,
+            render,
+            slots: _.omit(slots, 'default'),
+          }
           : {};
       }, [virtualize, data, render]);
       return result;
@@ -80,29 +135,72 @@ export default SelectBasicAccumulate.addPlugin({
   .addPlugin({
     name: 'handlePreview',
     handle(props) {
-      const ref = props.get('ref');
-      const Component = props.get('render');
       const isPreview = getIsPreview(props);
-      const data = props.get('data');
-      const modelValue = props.get('modelValue');
-      const valueList = _.isArray(modelValue) ? modelValue : [modelValue];
-      const previewText = _.join(
-        _.map(valueList, (item) => _.get(_.find(data, { value: item }), 'label', '')),
-        ',',
+      const className = props.get('class');
+
+      return isPreview
+        ? {
+          class: addClass(className, {
+            'el-select-preview': !!isPreview,
+          }),
+          disabled: !!isPreview,
+          preview: !!isPreview,
+        }
+        : {};
+    },
+  })
+
+  .addPlugin({
+    name: 'handleRemote',
+    handle(props) {
+      const remote = props.get('remote');
+      if (!remote) return {};
+      const emit = props.get('emit');
+      const remoteMethodProps = props.get('remoteMethod') ?? (() => { });
+      const onBeforeFilter = props.get('onBeforeFilter') ?? (() => { });
+      const ref = props.get('ref');
+      const remoteMethod = useCallback(
+        (fn, query: string) => {
+          emit('sync:state', 'filterText', query);
+          _.attempt(onBeforeFilter, { filterText: query });
+          _.attempt(ref?.reload);
+          _.attempt(remoteMethodProps, query);
+        },
+        [remoteMethodProps, onBeforeFilter],
       );
-
-      const previewRender = (insProps) => {
-        const inIDE = !!props.get('data-nodepath');
-        const previewText = inIDE || _.isEmpty(insProps.previewText) ? '-' : insProps.previewText;
-        return <ElPreview text={previewText} />;
-      };
-
-      const { render, insRef } = getRender(Component, previewRender, isPreview);
-
       return {
-        ref: Object.assign(ref, _.omit(insRef.value, ['reload', 'data'])),
-        render,
-        previewText,
+        remoteMethod,
+      };
+    },
+  })
+  .addPlugin({
+    name: 'handleSyncState',
+    handle(props) {
+      const emit = props.get('emit');
+      const data = props.get('data');
+      const disabled = props.get('disabled');
+      const preview = props.get('preview');
+      useEffect(() => {
+        emit('sync:state', 'data', data);
+        emit('sync:state', 'total', data.length);
+        emit('sync:state', 'disabled', disabled);
+        emit('sync:state', 'preview', preview);
+      }, [data, disabled, preview]);
+      return {};
+    },
+  })
+  .addPlugin({
+    name: 'handleIcon',
+    handle(props) {
+      const suffixIcon = props.get('suffixIcon');
+      const onClickSuffix = props.get('onClickSuffix', () => { });
+      return {
+        suffixIcon: getPropsIcon({
+          name: suffixIcon,
+          onClick: (e: Event) => {
+            _.attempt(onClickSuffix, e);
+          },
+        }),
       };
     },
   });
