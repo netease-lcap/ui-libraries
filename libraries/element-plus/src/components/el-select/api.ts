bf37b17b202c6fce207948d7a26e383490da24b3
= <512828461@qq.com>
2026-01-22 17:26:00 +0800

    feat: 合并 4.4select 功能到 4.1

--- libraries/element-plus/src/components/el-select/api.ts
+++ libraries/element-plus/src/components/el-select/api.ts
@@ -189,14 +189,6 @@ namespace nasl.ui {
     })
     multiple: M = false as any;
 
-    @Prop({
-      group: '主要属性',
-      title: '自动获取焦点',
-      description: '自动获取焦点',
-      setter: { concept: 'SwitchSetter' },
-    })
-    autoFocus: nasl.core.Boolean = false;
-
     @Prop<ElSelectOptions<T, V, P, M, C>, 'collapseTags'>({
       group: '主要属性',
       title: '折叠标签',
