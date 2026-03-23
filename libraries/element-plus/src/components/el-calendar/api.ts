bdbb63828d9428286181623c9e29a7fe6e63451d
= <512828461@qq.com>
2026-03-23 13:54:20 +0800

    feat(el-calendar): 添加日历组件的数据源支持和样式调整

--- libraries/element-plus/src/components/el-calendar/api.ts
+++ libraries/element-plus/src/components/el-calendar/api.ts
@@ -5,6 +5,9 @@ namespace nasl.ui {
     order: 2,
     ideusage: {
       idetype: 'element',
+      additionalAttribute: {
+        ':showInDesigner': '"true"',
+      },
     },
   })
   @Component({
@@ -13,13 +16,13 @@ namespace nasl.ui {
     description: '显示日期',
     group: 'Table',
   })
-  export class ElCalendar extends ViewComponent {
-    constructor(options?: Partial<ElCalendarOptions>) {
+  export class ElCalendar<T, V, P extends nasl.core.Boolean, M extends nasl.core.Boolean, C> extends ViewComponent {
+    constructor(options?: Partial<ElCalendarOptions<T, V, P, M, C>>) {
       super();
     }
   }
 
-  export class ElCalendarOptions extends ViewComponentOptions {
+  export class ElCalendarOptions<T, V, P extends nasl.core.Boolean, M extends nasl.core.Boolean, C> extends ViewComponentOptions {
     // ========== 数据来源相关属性 ==========
     @Prop({
       group: '数据属性',
@@ -40,16 +43,53 @@ namespace nasl.ui {
     })
     range: nasl.collection.List<nasl.core.Date | nasl.core.String>;
 
+    @Prop({
+      group: '数据属性',
+      title: '数据源',
+      description: '设置日历的数据来源，支持绑定集合类型变量或返回集合的逻辑',
+      docDescription:
+        '可以绑定 List<nasl.core.Date | nasl.core.String> 类型的变量，或者绑定返回 List<nasl.core.Date | nasl.core.String> 类型的逻辑。当使用数据源时，日历会根据数据动态生成日期，每个日期对应一个日期单元格。',
+      setter: {
+        concept: 'DataSourceSetter',
+      },
+    })
+    dataSource: { list: nasl.collection.List<nasl.core.Date | nasl.core.String>; total: nasl.core.Integer } | nasl.collection.List<nasl.core.Date | nasl.core.String>;
+
+    @Prop({
+      group: '数据属性',
+      title: '数据类型',
+      description: '数据源中每个数据项的类型定义，用于类型推导和属性选择',
+      docDescription: '此属性为只读，当绑定数据源后会自动识别数据项的类型T，用于在插槽中提供类型提示和属性选择器。',
+    })
+    dataSchema: T;
+
+    @Prop<ElCalendarOptions<T, V, P, M, C>, 'startKey'>({
+      group: '数据属性',
+      title: '开始时间字段',
+      description: '数据内表示开始时间的字段',
+      docDescription: '数据内表示开始时间的字段，要求对应数据必须包含日期（日期/日期时间格式），**单独使用表示当天**；跟结束时间字段配合使用表示日期区间，组件根据日期区间判断展示在哪些日期内。',
+      setter: {
+        concept: 'PropertySelectSetter',
+      },
+    })
+    startKey: (item: T) => any = ((item: any) => item.startTime) as any;
+
+    @Prop<ElCalendarOptions<T, V, P, M, C>, 'endKey'>({
+      group: '数据属性',
+      title: '结束时间字段',
+      description: '数据内表示结束时间的字段',
+      docDescription: '数据内表示结束时间的字段，要求对应数据必须包含日期（日期/日期时间格式），跟开始时间字段配合使用表示日期区间，组件根据时间区间判断展示在哪些日期内。',
+      setter: {
+        concept: 'PropertySelectSetter',
+      },
+    })
+    endKey: (item: T) => any = ((item: any) => item.endTime) as any;
+
     @Slot({
       title: '日期单元格',
       description: '自定义日期单元格内容。参数为 { type, isSelected, day, date }，type 表示该日期的所属月份，可选值有 prev-month、current-month、next-month；isSelected 标明该日期是否被选中；day 是格式化的日期，格式为 YYYY-MM-DD；date 是单元格的日期',
     })
-    slotDateCell: (current: {
-      type: 'prev-month' | 'current-month' | 'next-month';
-      isSelected: nasl.core.Boolean;
-      day: nasl.core.String;
-      date: nasl.core.Date;
-    }) => Array<ViewComponent>;
+    slotCell: (current: T) => Array<ViewComponent>;
 
     @Slot({
       title: '头部',
--- libraries/element-plus/src/components/el-calendar/plugins/index.tsx
+++ libraries/element-plus/src/components/el-calendar/plugins/index.tsx
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import dayjs from 'dayjs';
 import { CalendarProps } from 'element-plus';
-import { useMemo, useControllableValue } from '@/plugins/hooks';
+import { useMemo, useControllableValue, useCallback, useRef } from '@/plugins/hooks';
 import { PluginAccumulateTypes } from '@/plugins/accumulate';
 
-const CalendarAccumulate = new PluginAccumulateTypes<nasl.ui.ElCalendarOptions, CalendarProps>();
+import { useRequestDataSource, useHandleMapField, useFormatDataSource } from '@/plugins/common/dataSource';
+
+const CalendarAccumulate = new PluginAccumulateTypes<nasl.ui.ElCalendarOptions<any, any, any, any, any>, CalendarProps>();
 export default CalendarAccumulate.addPlugin({
   name: 'handleRange',
   handle: (props) => {
@@ -22,15 +24,71 @@ export default CalendarAccumulate.addPlugin({
     const [value, setValue] = useControllableValue(props);
     const modelValue = useMemo(
       () => _.match(value)
-          .when(_.isString, () => new Date(value))
-          .when(_.isDate, () => value)
-          .otherwise(() => value),
+        .when(_.isString, () => new Date(value))
+        .when(_.isDate, () => value)
+        .otherwise(() => value),
       [value],
     );
     return {
       modelValue,
       'onUpdate:modelValue': (value) => {
         setValue(dayjs(value).format('YYYY-MM-DD'));
+      },
+    };
+  },
+}).addPlugin({
+  name: 'handleDataSource',
+  handle: (props) => {
+    const dataConfig = props.get('dataSource');
+    const startKey = props.get('startKey') as unknown as string || 'startTime';
+    const endKey = props.get('endKey') as unknown as string || 'endTime';
+    const { data, run: reload, loading } = useRequestDataSource(dataConfig, {});
+    const dataSource = useHandleMapField({
+      dataSource: useFormatDataSource(data),
+      fieldsMap: {
+        startTime: startKey,
+        endTime: endKey,
+      },
+    });
+    return {
+      data: dataSource,
+    };
+  },
+}).addPlugin({
+  name: 'handleSlots',
+  handle: (props) => {
+    const slots = props.get('slots');
+    const showInDesigner = props.get('showInDesigner');
+    const cell = props.get('slotCell');
+    const data = props.get('data');
+    const dataRef = useRef(data);
+    dataRef.value = data;
+    const wrapCellRender = useCallback((data) => {
+      const dataIsRender = dataRef.value.some((item) => {
+        if (dayjs(item.startTime || '').isValid() && dayjs(item.endTime || '').isValid()) {
+          return dayjs(data.day).isSame(item.startTime, 'day')
+            || dayjs(data.day).isSame(item.endTime, 'day')
+            || (dayjs(data.day).isAfter(item.startTime, 'day') && dayjs(data.day).isBefore(item.endTime, 'day'));
+        }
+        return dayjs(data.day).isSame(item.startTime, 'day') || dayjs(data.day).isSame(item.endTime, 'day');
+      });
+      const isRender = showInDesigner ? true : dataIsRender;
+      return isRender ? cell(data) : null;
+    }, [cell]);
+    const dateCell = useCallback(({ data }) => {
+      return (
+        <div class="cw-date-cell">
+          <div class={data.isSelected ? 'is-selected' : ''}>
+            {dayjs(data.day).format('D')}
+            {wrapCellRender(data)}
+          </div>
+        </div>
+      );
+    }, []);
+    return {
+      slots: {
+        ...slots,
+        'date-cell': dateCell,
       },
     };
   },
