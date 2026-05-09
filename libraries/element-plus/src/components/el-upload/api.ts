64705fcb7fa5819c9b9c188406caf8726a8c8b38
= <songrui01@corp.netease.com>
2026-05-09 14:29:30 +0800

    fix(el-upload): 更新上传组件的 URL 属性，修复属性名称不一致的问题

--- libraries/element-plus/src/components/el-upload/api.ts
+++ libraries/element-plus/src/components/el-upload/api.ts
@@ -170,7 +170,7 @@ namespace nasl.ui {
       description: '文件上传的服务器地址',
       docDescription: '设置文件上传的服务器地址URL，支持相对路径和绝对路径。',
       setter: { concept: 'InputSetter' },
-      if: (_) => false,
+      if: (_) => true,
     })
     action: nasl.core.String = '/upload';
 
